05c319e0f2f13aafdf0434eaf82f43227d3b06b8
Brad Greenlee <brad@etsy.com>
2017-12-03 09:21:39 -0800

    Move git to parent dir

---
+++ 2016/day-01/src/day_01/core.clj
@@ -0,0 +1,16 @@
+(ns day-01.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(println "Cleanliness is next to godliness")
+
+(defn train
+  []
+  (println "Choo choo!"))
+
+
+
