43bd4bd5bcb30d72b778ade135e35cfcbb58f221
Brad Greenlee <brad@footle.org>
2018-12-01 07:41:15 -0800

    Parse the list of numbers only once

--- 2018/01-chronal-calibration/chronal-calibration-2/src/main.rs
+++ 2018/01-chronal-calibration/chronal-calibration-2/src/main.rs
@@ -2,20 +2,20 @@ use std::io::{self, Read};
 use std::collections::HashSet;
 
 fn main() -> io::Result<()>  {
-    // read input frequencies into a string
-    let mut frequencies = String::new();
-    io::stdin().read_to_string(&mut frequencies)?;
+    // read input frequency changes into a vector
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    let freq_changes: Vec<i32> = buffer.lines().map(|l|
+        l.parse::<i32>().unwrap_or(0)
+    ).collect();
 
     let mut frequency = 0;
     let mut seen_frequencies = HashSet::new();
     let mut found_duplicate = false;
 
     while !found_duplicate {
-        for line in frequencies.lines() {
-            match line.parse::<i32>() {
-                Ok(n) => frequency += n,
-                Err(_) => (), // ignore values that can't be parsed to ints
-            }
+        for freq_change in freq_changes.iter() {
+            frequency += freq_change;
             if seen_frequencies.contains(&frequency) {
                 println!("First duplicate: {}", frequency);
                 found_duplicate = true;
