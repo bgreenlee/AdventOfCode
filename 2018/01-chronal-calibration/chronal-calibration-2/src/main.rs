99b8ca2493ed3f9898de91ac13ea9acb3e4c063a
Brad Greenlee <brad@footle.org>
2018-12-01 20:55:45 -0800

    Refactor & comment

--- 2018/01-chronal-calibration/chronal-calibration-2/src/main.rs
+++ 2018/01-chronal-calibration/chronal-calibration-2/src/main.rs
@@ -1,6 +1,24 @@
 use std::io::{self, Read};
 use std::collections::HashSet;
 
+/// Finds the first duplicate frequency starting with the initial frequency
+/// and applying the changes from the given vector of frequency changes
+/// repeatedly.
+fn find_first_duplicate(mut frequency: i32, freq_changes: Vec<i32>) -> i32 {
+    let mut seen_frequencies = HashSet::new();
+    loop {
+        for freq_change in freq_changes.iter() {
+            frequency += freq_change;
+            if seen_frequencies.contains(&frequency) {
+                return frequency;
+            }
+            seen_frequencies.insert(frequency);
+        }
+    }
+}
+
+/// Read a list of frequency changes from stdin and print the first
+/// duplicate frequency.
 fn main() -> io::Result<()>  {
     // read input frequency changes into a vector
     let mut buffer = String::new();
@@ -9,20 +27,7 @@ fn main() -> io::Result<()>  {
         l.parse::<i32>().unwrap_or(0)
     ).collect();
 
-    let mut frequency = 0;
-    let mut seen_frequencies = HashSet::new();
-    let mut found_duplicate = false;
-
-    while !found_duplicate {
-        for freq_change in freq_changes.iter() {
-            frequency += freq_change;
-            if seen_frequencies.contains(&frequency) {
-                println!("First duplicate: {}", frequency);
-                found_duplicate = true;
-                break;
-            }
-            seen_frequencies.insert(frequency);
-        }
-    }
+    let duplicate = find_first_duplicate(0, freq_changes);
+    println!("First duplicate: {}", duplicate);
     Ok(())
 }
