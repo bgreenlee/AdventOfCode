d2e44d238ca69b58f741791ca1264282506f1f3b
Brad Greenlee <brad@footle.org>
2018-12-01 21:14:27 -0800

    Add test

--- 2018/01-chronal-calibration/chronal-calibration-2/src/main.rs
+++ 2018/01-chronal-calibration/chronal-calibration-2/src/main.rs
@@ -1,9 +1,21 @@
 use std::io::{self, Read};
 use std::collections::HashSet;
 
-/// Finds the first duplicate frequency starting with the initial frequency
-/// and applying the changes from the given vector of frequency changes
-/// repeatedly.
+/// Finds the first duplicate frequency.
+///
+/// Starting with the initial frequency and applying the changes from
+/// the given vector of frequency changes repeatedly, return the first
+/// frequency value that repeats.
+///
+/// There's no guarantee that this will ever finish. There should
+/// probably be some sort of timeout, but this is only day 1.
+///
+/// # Example
+///
+/// ```
+/// let result = doccomments::find_first_duplicate(0, vec![+3, +3, +4, -2, -4]);
+/// assert_eq(result, 10);
+/// ```
 fn find_first_duplicate(mut frequency: i32, freq_changes: Vec<i32>) -> i32 {
     let mut seen_frequencies = HashSet::new();
     loop {
