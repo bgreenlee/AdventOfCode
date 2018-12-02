d78774d5b9eee82ec6a169053308bf6b967d71b9
Brad Greenlee <brad@footle.org>
2018-12-02 07:19:43 -0800

    More fixing tests

--- 2018/01-chronal-calibration/chronal-calibration-2/src/main.rs
+++ 2018/01-chronal-calibration/chronal-calibration-2/src/main.rs
@@ -9,13 +9,6 @@ use std::collections::HashSet;
 ///
 /// There's no guarantee that this will ever finish. There should
 /// probably be some sort of timeout, but this is only day 1.
-///
-/// # Example
-///
-/// ```
-/// let result = doccomments::find_first_duplicate(0, [+3, +3, +4, -2, -4]);
-/// assert_eq(result, 10);
-/// ```
 fn find_first_duplicate(mut frequency: i32, freq_changes: &[i32]) -> i32 {
     let mut seen_frequencies = HashSet::new();
     loop {
@@ -42,4 +35,15 @@ fn main() -> io::Result<()>  {
     let duplicate = find_first_duplicate(0, &freq_changes);
     println!("First duplicate: {}", duplicate);
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_first_duplicate() {
+        let result = find_first_duplicate(0, &[3, 3, 4, -2, -4]);
+        assert_eq!(result, 10);
+    }
 }
