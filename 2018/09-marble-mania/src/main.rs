58711ab0c89f79116db8b6942c12acd2365cc80c
Brad Greenlee <brad@footle.org>
2018-12-11 15:18:34 -0800

    use i64s

--- 2018/09-marble-mania/src/main.rs
+++ 2018/09-marble-mania/src/main.rs
@@ -9,18 +9,18 @@ fn main() {
     }
 
     let num_players = env::args().nth(1).unwrap()
-        .parse::<i32>().expect("Could not parse input.");
+        .parse::<i64>().expect("Could not parse input.");
     let last_marble_value = env::args().nth(2).unwrap()
-        .parse::<i32>().expect("Could not parse input.");
+        .parse::<i64>().expect("Could not parse input.");
 
-    let mut circle : VecDeque<i32> = VecDeque::with_capacity(last_marble_value as usize);
-    let mut current_marble_idx : i32 = 0;
+    let mut circle : VecDeque<i64> = VecDeque::with_capacity(last_marble_value as usize);
+    let mut current_marble_idx : i64 = 0;
     let mut player_scores = vec![0; num_players as usize];
 
     circle.push_back(0);
+    let mut circle_len : i64 = 1;
 
     for current_marble_value in 1..=last_marble_value {
-        let circle_len = circle.len() as i32;
         if current_marble_value % 23 != 0 {
             let next_marble_idx = ((current_marble_idx + 1) % circle_len) + 1;
             if next_marble_idx == circle_len {
@@ -28,12 +28,14 @@ fn main() {
             } else {
                 circle.insert(next_marble_idx as usize, current_marble_value);
             }
+            circle_len += 1;
             current_marble_idx = next_marble_idx;
         } else {
             let player_num = current_marble_value % num_players;
             player_scores[player_num as usize] += current_marble_value;
-            let bonus_marble_idx : i32 = (current_marble_idx - 7 + circle_len) % circle_len;
+            let bonus_marble_idx : i64 = (current_marble_idx - 7 + circle_len) % circle_len;
             let bonus_marble = circle.remove(bonus_marble_idx as usize).unwrap();
+            circle_len -= 1;
             player_scores[player_num as usize] += bonus_marble;
             current_marble_idx = bonus_marble_idx;
         }
