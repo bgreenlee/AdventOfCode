5929cd0595c7c5e0b46e95fe0c22e99922cf7096
Brad Greenlee <brad@etsy.com>
2018-12-11 14:36:02 -0800

    Day 9, part 2, before rewriting to use a LinkedList

--- 2018/09-marble-mania/src/main.rs
+++ 2018/09-marble-mania/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::process::exit;
+use std::collections::VecDeque;
 
 fn main() {
     if env::args().len() < 3 {
@@ -12,17 +13,18 @@ fn main() {
     let last_marble_value = env::args().nth(2).unwrap()
         .parse::<i32>().expect("Could not parse input.");
 
-    let mut circle : Vec<i32> = Vec::new();
+    let mut circle : VecDeque<i32> = VecDeque::with_capacity(last_marble_value as usize);
     let mut current_marble_idx : i32 = 0;
     let mut player_scores = vec![0; num_players as usize];
 
-    circle.push(0);
+    circle.push_back(0);
 
     for current_marble_value in 1..=last_marble_value {
+        let circle_len = circle.len() as i32;
         if current_marble_value % 23 != 0 {
-            let next_marble_idx = ((current_marble_idx + 1) % circle.len() as i32) + 1;
-            if next_marble_idx == circle.len() as i32 {
-                circle.push(current_marble_value);
+            let next_marble_idx = ((current_marble_idx + 1) % circle_len) + 1;
+            if next_marble_idx == circle_len {
+                circle.push_back(current_marble_value);
             } else {
                 circle.insert(next_marble_idx as usize, current_marble_value);
             }
@@ -30,14 +32,17 @@ fn main() {
         } else {
             let player_num = current_marble_value % num_players;
             player_scores[player_num as usize] += current_marble_value;
-            let bonus_marble_idx : i32 = (current_marble_idx - 7 + circle.len() as i32) % circle.len() as i32;
-            let bonus_marble = circle.remove(bonus_marble_idx as usize);
+            let bonus_marble_idx : i32 = (current_marble_idx - 7 + circle_len) % circle_len;
+            let bonus_marble = circle.remove(bonus_marble_idx as usize).unwrap();
             player_scores[player_num as usize] += bonus_marble;
             current_marble_idx = bonus_marble_idx;
         }
+        let pct_done = (current_marble_value as f64 / last_marble_value as f64) * 100.0;
+        if current_marble_value % (last_marble_value / 100) == 0 {
+            println!("{:.*}%", 0, pct_done);
+        }
     }
 
-    println!("scores: {:?}", player_scores);
     let top_score = player_scores.iter().max().unwrap_or(&0);
     println!("top score: {}", top_score);
 }
