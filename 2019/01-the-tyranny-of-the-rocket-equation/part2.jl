a6c55d6a8c2f5c00f57e0600cdcfc66823e65563
Brad Greenlee <brad@footle.org>
2019-12-02 19:06:53 -0800

    Part 2

---
+++ 2019/01-the-tyranny-of-the-rocket-equation/part2.jl
@@ -0,0 +1,17 @@
+#!env julia
+
+# read in the list of masses from stdin and return an array of Ints
+function readmasses()
+    masses = map(n -> tryparse(Int, n), readlines())
+    filter(n -> n != nothing, masses) # remove any bad input
+end
+
+# calculate the fuel required given the mass
+function fuel(m)
+    f = floor(Int, m/3) - 2
+    f <= 0 ? 0 : f + fuel(f) # recursively include the fuel needed to carry the fuel
+end
+
+masses = readmasses()
+total = sum(fuel, masses)
+print("Total: $total\n")
