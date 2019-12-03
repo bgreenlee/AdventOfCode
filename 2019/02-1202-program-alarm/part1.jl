38659e99815aa36c4ffbbe1df51f4927bf4646c6
Brad Greenlee <brad@footle.org>
2019-12-03 05:53:48 -0800

    day 2, part 1

---
+++ 2019/02-1202-program-alarm/part1.jl
@@ -0,0 +1,34 @@
+#!env julia
+
+# read the Intcode program from stdin and return an array of Ints
+function readprogram()
+    opcodes = split(readline(), ",")
+    opcodes = map(n -> tryparse(Int, n), opcodes)
+    filter(n -> n != nothing, opcodes) # remove any bad input
+end
+
+mem = readprogram()
+# patch per instructions
+mem[2] = 12
+mem[3] = 2
+# Julia uses 1-based indexing
+ptr = 1
+
+while ptr <= length(mem)
+    print("$ptr -> $mem\n")
+    global ptr
+    if mem[ptr] == 1
+        mem[mem[ptr+3]+1] = mem[mem[ptr+1]+1] + mem[mem[ptr+2]+1]
+        ptr += 4
+    elseif mem[ptr] == 2
+        mem[mem[ptr+3]+1] = mem[mem[ptr+1]+1] * mem[mem[ptr+2]+1]
+        ptr += 4
+    elseif mem[ptr] == 99
+        break
+    else
+        print("unknown opcode: $(mem[ptr]) at #$ptr\n")
+    end
+end
+
+print("mem[1] = $(mem[1])\n")
+
