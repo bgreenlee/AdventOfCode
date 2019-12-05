411eb08a415a7a8821bbaad7bf4f8bae408e3614
Brad Greenlee <brad@etsy.com>
2019-12-04 16:06:21 -0800

    Code golfing

---
+++ 2019/04-secure-container/part2-golf.jl
@@ -0,0 +1,12 @@
+#!/usr/bin/env julia
+
+function isvalid(n::Int64)::Bool
+    digits = map(d -> d[1], split(string(n), "")) # map number to array of chars
+    digits == sort(digits) && findfirst(c -> c == 2, [count(a -> a == b, digits) for b in '0':'9']) != nothing
+end
+
+start_num = parse(Int, ARGS[1])
+end_num = parse(Int, ARGS[2])
+
+valid = filter(isvalid, start_num:end_num)
+println("Num valid: $(length(valid))")
