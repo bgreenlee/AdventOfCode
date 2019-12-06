741ae3485cda67ccf45ad8ff604294431736c73a
Brad Greenlee <brad@etsy.com>
2019-12-06 06:00:06 -0800

    Realized the "ancestors" would mean parent on up

--- 2019/06-universal-orbital-map/part1.jl
+++ 2019/06-universal-orbital-map/part1.jl
@@ -8,10 +8,10 @@ end
 leaf(n) = Node(n, [])
 addchild!(n::Node, c::Node) = push!(n.children, c)
 
-function ancestors(n::Node)
+function subnodes(n::Node)
     a = copy(n.children)
     for c in n.children
-        append!(a, ancestors(c))
+        append!(a, subnodes(c))
     end
     a
 end
@@ -31,6 +31,6 @@ end
 
 map = readlines(ARGS[1])
 tree = generate_tree(map)
-all_nodes = ancestors(tree["COM"])
-total = length(all_nodes) + sum(n -> length(ancestors(n)), all_nodes)
+all_nodes = subnodes(tree["COM"])
+total = length(all_nodes) + sum(n -> length(subnodes(n)), all_nodes)
 println(total)
