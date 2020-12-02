f337b35ccb85d811c825dadc4756ccaa8f9e50d0
Brad Greenlee <brad@footle.org>
2020-12-01 21:17:38 -0800

    Day 1, part 2

--- 2020/01-report-repair/Sources/ReportRepair/main.swift
+++ 2020/01-report-repair/Sources/ReportRepair/main.swift
@@ -5,10 +5,26 @@ while let line = readLine(strippingNewline: true) {
     numbers.update(with: Int(line) ?? 0)
 }
 
+func findNumbers(in numbers: Set<Int>, summingTo target: Int) -> (Int, Int)? {
+    for number in numbers {
+        let diff = target - number
+        if numbers.contains(diff) {
+            return (number, diff)
+        }
+    }
+    return nil
+}
+
+// Part 1
+if let (a,b) = findNumbers(in: numbers, summingTo: target) {
+    print("\(a) * \(b) = \(a * b)")
+}
+
+// Part 2
 for number in numbers {
     let diff = target - number
-    if numbers.contains(diff) {
-        print("\(number) * \(diff) = \(number * diff)\n")
+    if let (a,b) = findNumbers(in: numbers, summingTo: diff) {
+        print("\(number) * \(a) * \(b) = \(number * a * b)")
         break
     }
 }
