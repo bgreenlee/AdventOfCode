25cd1012814ab8131005bda02464e052f6e1cc02
Brad Greenlee <brad@etsy.com>
2020-12-04 06:01:00 -0800

    Saner regexing

--- 2020/Sources/04-PassportProcessing/Part1.swift
+++ 2020/Sources/04-PassportProcessing/Part1.swift
@@ -3,9 +3,9 @@ import Foundation
 struct Part1 {
     static func run(with records: [String]) {
         // match valid fields in each line
-        let validFieldsRegex = try! NSRegularExpression(pattern: "(byr|iyr|eyr|hgt|hcl|ecl|pid):")
+        let validFieldsRegex = try! RegEx(pattern: "(byr|iyr|eyr|hgt|hcl|ecl|pid):")
         let numValid = records.filter {
-            validFieldsRegex.numberOfMatches(in: $0, options: [], range: NSRange(location: 0, length: $0.count)) == 7
+            validFieldsRegex.matchGroups(in: $0).count == 7
         }.count
         print("Valid: \(numValid)")
     }
--- 2020/Sources/04-PassportProcessing/Part2.swift
+++ 2020/Sources/04-PassportProcessing/Part2.swift
@@ -3,14 +3,14 @@ import Foundation
 struct Part2 {
     static func run(with records: [String]) {
         // match valid fields in each line
-        let fieldsRegex = try! NSRegularExpression(pattern: "(byr|iyr|eyr|hgt|hcl|ecl|pid):(\\S+)")
+        let fieldsRegex = try! RegEx(pattern: "(byr|iyr|eyr|hgt|hcl|ecl|pid):(\\S+)")
         var validRecords = 0
         for record in records {
-            let matches = fieldsRegex.matches(in: record, options: [], range: NSRange(location: 0, length: record.count))
+            let matches = fieldsRegex.matchGroups(in: record)
             var validFields = 0
             for match in matches {
-                let code = record[Range(match.range(at: 1), in:record)!] // yuck, Swift
-                let value = String(record[Range(match.range(at: 2), in:record)!])
+                let code = match[0]
+                let value = match[1]
                 switch code {
                 case "byr":
                     if value.count == 4 {
@@ -34,10 +34,11 @@ struct Part2 {
                         }
                     }
                 case "hgt":
-                    let validRegex = try! NSRegularExpression(pattern: "^(\\d+)(cm|in)$")
-                    if let hgtMatch = validRegex.firstMatch(in: value, options: [], range: NSRange(location: 0, length: value.count)) {
-                        let height = Int(value[Range(hgtMatch.range(at: 1), in:value)!]) ?? 0
-                        let unit = value[Range(hgtMatch.range(at: 2), in:value)!]
+                    let validRegex = try! RegEx(pattern: "^(\\d+)(cm|in)$")
+                    let hgtMatch = validRegex.matchGroups(in: value)
+                    if hgtMatch.count == 1 {
+                        let height = Int(hgtMatch.first![0]) ?? 0
+                        let unit = hgtMatch.first![1]
                         if unit == "cm" && height >= 150 && height <= 193 {
                             validFields += 1
                         } else if unit == "in" && height >= 59 && height <= 76 {
@@ -45,18 +46,18 @@ struct Part2 {
                         }
                     }
                 case "hcl":
-                    let validRegex = try! NSRegularExpression(pattern: "^#[0-9a-f]{6}$")
-                    if validRegex.numberOfMatches(in: value, options: [], range: NSRange(location: 0, length: value.count)) == 1 {
+                    let validRegex = try! RegEx(pattern: "^#[0-9a-f]{6}$")
+                    if validRegex.matchGroups(in: value).count == 1 {
                         validFields += 1
                     }
                 case "ecl":
-                    let validRegex = try! NSRegularExpression(pattern: "^amb|blu|brn|gry|grn|hzl|oth$")
-                    if validRegex.numberOfMatches(in: value, options: [], range: NSRange(location: 0, length: value.count)) == 1 {
+                    let validRegex = try! RegEx(pattern: "^amb|blu|brn|gry|grn|hzl|oth$")
+                    if validRegex.matchGroups(in: value).count == 1 {
                         validFields += 1
                     }
                 case "pid":
-                    let validRegex = try! NSRegularExpression(pattern: "^\\d{9}$")
-                    if validRegex.numberOfMatches(in: value, options: [], range: NSRange(location: 0, length: value.count)) == 1 {
+                    let validRegex = try! RegEx(pattern: "^\\d{9}$")
+                    if validRegex.matchGroups(in: value).count == 1 {
                         validFields += 1
                     }
                 default:
