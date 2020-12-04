fe5e7c4080b83ba656461ff383de82592bd18f9f
Brad Greenlee <brad@etsy.com>
2020-12-04 05:48:41 -0800

    Simplify part 1

--- 2020/Sources/04-PassportProcessing/Part1.swift
+++ 2020/Sources/04-PassportProcessing/Part1.swift
@@ -4,13 +4,9 @@ struct Part1 {
     static func run(with records: [String]) {
         // match valid fields in each line
         let validFieldsRegex = try! NSRegularExpression(pattern: "(byr|iyr|eyr|hgt|hcl|ecl|pid):")
-        var numValid = 0
-        for record in records {
-            let numMatches = validFieldsRegex.numberOfMatches(in: record, options: [], range: NSRange(location: 0, length: record.count))
-            if numMatches == 7 {
-                numValid += 1
-            }
-        }
+        let numValid = records.filter {
+            validFieldsRegex.numberOfMatches(in: $0, options: [], range: NSRange(location: 0, length: $0.count)) == 7
+        }.count
         print("Valid: \(numValid)")
     }
 }
