e49764f44129c439f7efc3ec625f17b4b6bc8b7c
Brad Greenlee <brad@footle.org>
2020-12-05 15:29:39 -0800

    Simplify further

--- 2020/Sources/05-BinaryBoarding/main.swift
+++ 2020/Sources/05-BinaryBoarding/main.swift
@@ -2,9 +2,15 @@ import Foundation
 
 if let inputURL = Bundle.module.url(forResource: "input", withExtension: "dat", subdirectory: "data") {
     let input = try String(contentsOf: inputURL)
-    let boardingPassIds = input.components(separatedBy: .newlines)
-                            .compactMap { BoardingPass($0)?.id }
-                            .sorted()
+    let boardingPassIds:[Int] = input.components(separatedBy: .newlines)
+        .compactMap {
+            let binary = $0.replacingOccurrences(of: "F", with: "0")
+                           .replacingOccurrences(of: "B", with: "1")
+                           .replacingOccurrences(of: "L", with: "0")
+                           .replacingOccurrences(of: "R", with: "1")
+            return Int(binary, radix: 2)
+        }
+        .sorted()
 
     // Part 1: find the highest id
     print("Part 1: \(boardingPassIds.last!)")
