825c7d400abe2958f88781e3b2209e80f93c679c
Brad Greenlee <brad@footle.org>
2020-12-05 05:29:13 -0800

    Don't need StringExtensions

--- 2020/Sources/05-BinaryBoarding/StringExtensions.swift
+++
@@ -1,9 +0,0 @@
-extension StringProtocol {
-    // make indexing into Strings less annoying
-    subscript(offset: Int) -> Character {
-        self[index(startIndex, offsetBy: offset)]
-    }
-    
-//    subscript(range: Range)
-}
-
