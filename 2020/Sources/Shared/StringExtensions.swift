21654ec77494539aa7337451c56ae4bc74acff35
Brad Greenlee <brad@footle.org>
2020-12-12 04:13:37 -0800

    Reformat StringExtensions

--- 2020/Sources/Shared/StringExtensions.swift
+++ 2020/Sources/Shared/StringExtensions.swift
@@ -1,14 +1,27 @@
-extension StringProtocol {
-    public subscript(offset: Int) -> Character { self[index(startIndex, offsetBy: offset)] }
-    public subscript(range: Range<Int>) -> SubSequence {
-        let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
-        return self[startIndex..<index(startIndex, offsetBy: range.count)]
+public extension StringProtocol {
+    subscript(offset: Int) -> Character {
+        self[index(startIndex, offsetBy: offset)]
     }
-    public subscript(range: ClosedRange<Int>) -> SubSequence {
+
+    subscript(range: Range<Int>) -> SubSequence {
         let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
-        return self[startIndex..<index(startIndex, offsetBy: range.count)]
+        return self[startIndex ..< index(startIndex, offsetBy: range.count)]
     }
-    public subscript(range: PartialRangeFrom<Int>) -> SubSequence { self[index(startIndex, offsetBy: range.lowerBound)...] }
-    public subscript(range: PartialRangeThrough<Int>) -> SubSequence { self[...index(startIndex, offsetBy: range.upperBound)] }
-    public subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
+
+    subscript(range: ClosedRange<Int>) -> SubSequence {
+        let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+        return self[startIndex ..< index(startIndex, offsetBy: range.count)]
+    }
+
+    subscript(range: PartialRangeFrom<Int>) -> SubSequence {
+        self[index(startIndex, offsetBy: range.lowerBound)...]
+    }
+
+    subscript(range: PartialRangeThrough<Int>) -> SubSequence {
+        self[...index(startIndex, offsetBy: range.upperBound)]
+    }
+
+    subscript(range: PartialRangeUpTo<Int>) -> SubSequence {
+        self[..<index(startIndex, offsetBy: range.upperBound)]
+    }
 }
