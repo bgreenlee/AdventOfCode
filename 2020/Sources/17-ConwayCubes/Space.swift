b4df3274c782e5a02e447208aa53d6cba800461d
Brad Greenlee <brad@etsy.com>
2020-12-17 09:16:47 -0800

    Some optimizations

--- 2020/Sources/17-ConwayCubes/Space.swift
+++ 2020/Sources/17-ConwayCubes/Space.swift
@@ -14,18 +14,18 @@ struct Space3D {
     }
 
     func neighbors(of point: Point3D) -> Set<Point3D> {
-        var neighbors:Set<Point3D> = []
+        var neighbors:[Point3D] = []
+        neighbors.reserveCapacity(26)
         for x in -1...1 {
             for y in -1...1 {
                 for z in -1...1 {
-                    if x == 0 && y == 0 && z == 0 {
-                        continue
+                    if (x,y,z) != (0,0,0) {
+                        neighbors.append(point + (x,y,z))
                     }
-                    neighbors.update(with: point + (x,y,z))
                 }
             }
         }
-        return neighbors
+        return Set(neighbors)
     }
 
     func activeNeighbors(of point: Point3D) -> Set<Point3D> {
@@ -36,24 +36,26 @@ struct Space3D {
         return neighbors(of: point).subtracting(cubes)
     }
 
-    mutating func cycle() {
-        var newCubes:Set<Point3D> = []
-        for cube in cubes {
-            // active cubes
-            let numActiveNeighbors = activeNeighbors(of: cube).count
-            if numActiveNeighbors == 2 || numActiveNeighbors == 3 {
-                newCubes.update(with: cube)
-            }
+    mutating func cycle(_ n:Int=1) {
+        for _ in 0..<n {
+            var newCubes:Set<Point3D> = []
+            for cube in cubes {
+                // active cubes
+                let numActiveNeighbors = activeNeighbors(of: cube).count
+                if numActiveNeighbors == 2 || numActiveNeighbors == 3 {
+                    newCubes.update(with: cube)
+                }
 
-            // inactive cubes
-            for inactiveCube in inactiveNeighbors(of: cube) {
-                let numActiveNeighbors = activeNeighbors(of: inactiveCube).count
-                if numActiveNeighbors == 3 {
-                    newCubes.update(with: inactiveCube)
+                // inactive cubes
+                for inactiveCube in inactiveNeighbors(of: cube) {
+                    let numActiveNeighbors = activeNeighbors(of: inactiveCube).count
+                    if numActiveNeighbors == 3 {
+                        newCubes.update(with: inactiveCube)
+                    }
                 }
             }
+            cubes = newCubes
         }
-        cubes = newCubes
     }
 }
 
@@ -71,20 +73,20 @@ struct Space4D {
     }
 
     func neighbors(of point: Point4D) -> Set<Point4D> {
-        var neighbors:Set<Point4D> = []
+        var neighbors:[Point4D] = []
+        neighbors.reserveCapacity(80)
         for x in -1...1 {
             for y in -1...1 {
                 for z in -1...1 {
                     for w in -1...1 {
-                        if x == 0 && y == 0 && z == 0 && w == 0 {
-                            continue
+                        if (x,y,z,w) != (0,0,0,0) {
+                            neighbors.append(point + (x,y,z,w))
                         }
-                        neighbors.update(with: point + (x,y,z,w))
                     }
                 }
             }
         }
-        return neighbors
+        return Set(neighbors)
     }
 
     func activeNeighbors(of point: Point4D) -> Set<Point4D> {
@@ -95,23 +97,25 @@ struct Space4D {
         return neighbors(of: point).subtracting(cubes)
     }
 
-    mutating func cycle() {
-        var newCubes:Set<Point4D> = []
-        for cube in cubes {
-            // active cubes
-            let numActiveNeighbors = activeNeighbors(of: cube).count
-            if numActiveNeighbors == 2 || numActiveNeighbors == 3 {
-                newCubes.update(with: cube)
-            }
+    mutating func cycle(_ n:Int=1) {
+        for _ in 0..<n {
+            var newCubes:Set<Point4D> = []
+            for cube in cubes {
+                // active cubes
+                let numActiveNeighbors = activeNeighbors(of: cube).count
+                if numActiveNeighbors == 2 || numActiveNeighbors == 3 {
+                    newCubes.update(with: cube)
+                }
 
-            // inactive cubes
-            for inactiveCube in inactiveNeighbors(of: cube) {
-                let numActiveNeighbors = activeNeighbors(of: inactiveCube).count
-                if numActiveNeighbors == 3 {
-                    newCubes.update(with: inactiveCube)
+                // inactive cubes
+                for inactiveCube in inactiveNeighbors(of: cube) {
+                    let numActiveNeighbors = activeNeighbors(of: inactiveCube).count
+                    if numActiveNeighbors == 3 {
+                        newCubes.update(with: inactiveCube)
+                    }
                 }
             }
+            cubes = newCubes
         }
-        cubes = newCubes
     }
 }
--- 2020/Sources/17-ConwayCubes/main.swift
+++ 2020/Sources/17-ConwayCubes/main.swift
@@ -7,15 +7,13 @@ if let input = try Bundle.module.readFile("data/input.dat") {
                        .filter { $0.count > 0 }
 
     var space3d = Space3D(rows)
-    for _ in 1...6 {
-        space3d.cycle()
-    }
+    space3d.cycle(6)
     print("Part 1: \(space3d.cubes.count)")
 
     var space4d = Space4D(rows)
-    for _ in 1...6 {
-        space4d.cycle()
-    }
+    let startTime = CFAbsoluteTimeGetCurrent()
+    space4d.cycle(6)
+    print("time: \(CFAbsoluteTimeGetCurrent() - startTime)")
     print("Part 2: \(space4d.cubes.count)")
 
 
