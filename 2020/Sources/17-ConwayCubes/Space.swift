e46d375a3f055e9138fae8451bf00cee430857ca
Brad Greenlee <brad@footle.org>
2020-12-17 20:49:51 -0800

    Trying to speed this up

--- 2020/Sources/17-ConwayCubes/Space.swift
+++ 2020/Sources/17-ConwayCubes/Space.swift
@@ -1,4 +1,5 @@
 import Foundation
+import simd
 
 struct Space3D {
     var cubes:Set<Point3D> = []
@@ -60,62 +61,67 @@ struct Space3D {
 }
 
 struct Space4D {
-    var cubes:Set<Point4D> = []
+    var cubes:Set<simd_int4> = []
+    var neighborCache:Dictionary<simd_int4,Set<simd_int4>> = [:]
 
     init(_ rows:[String]) {
         for (y, row) in rows.enumerated() {
             for (x, char) in row.enumerated() {
                 if char == "#" {
-                    cubes.update(with: Point4D(x,y,0,0))
+                    cubes.update(with: simd_make_int4(Int32(x),Int32(y),0,0))
                 }
             }
         }
     }
 
-    func neighbors(of point: Point4D) -> Set<Point4D> {
-        var neighbors:[Point4D] = []
+    mutating func neighbors(of point: simd_int4) -> Set<simd_int4> {
+        if let cached = neighborCache[point] {
+            return cached
+        }
+
+        var neighbors:[simd_int4] = []
         neighbors.reserveCapacity(80)
-        for x in -1...1 {
-            for y in -1...1 {
-                for z in -1...1 {
-                    for w in -1...1 {
+        for x:Int32 in -1...1 {
+            for y:Int32 in -1...1 {
+                for z:Int32 in -1...1 {
+                    for w:Int32 in -1...1 {
                         if (x,y,z,w) != (0,0,0,0) {
-                            neighbors.append(point + (x,y,z,w))
+                            neighbors.append(point &+ simd_make_int4(x,y,z,w))
                         }
                     }
                 }
             }
         }
-        return Set(neighbors)
+        neighborCache[point] = Set(neighbors)
+        return neighborCache[point]!
     }
 
-    func activeNeighbors(of point: Point4D) -> Set<Point4D> {
+    mutating func activeNeighbors(of point: simd_int4) -> Set<simd_int4> {
         return neighbors(of: point).intersection(cubes)
     }
 
-    func inactiveNeighbors(of point: Point4D) -> Set<Point4D> {
+    mutating func inactiveNeighbors(of point: simd_int4) -> Set<simd_int4> {
         return neighbors(of: point).subtracting(cubes)
     }
 
     mutating func cycle(_ n:Int=1) {
         for _ in 0..<n {
-            var newCubes:Set<Point4D> = []
+            var newCubes:[simd_int4] = []
             for cube in cubes {
                 // active cubes
                 let numActiveNeighbors = activeNeighbors(of: cube).count
                 if numActiveNeighbors == 2 || numActiveNeighbors == 3 {
-                    newCubes.update(with: cube)
+                    newCubes.append(cube)
                 }
 
                 // inactive cubes
                 for inactiveCube in inactiveNeighbors(of: cube) {
-                    let numActiveNeighbors = activeNeighbors(of: inactiveCube).count
-                    if numActiveNeighbors == 3 {
-                        newCubes.update(with: inactiveCube)
+                    if activeNeighbors(of: inactiveCube).count == 3 {
+                        newCubes.append(inactiveCube)
                     }
                 }
             }
-            cubes = newCubes
+            cubes = Set(newCubes)
         }
     }
 }
--- 2020/Sources/17-ConwayCubes/main.swift
+++ 2020/Sources/17-ConwayCubes/main.swift
@@ -6,15 +6,17 @@ if let input = try Bundle.module.readFile("data/input.dat") {
     let rows = input.components(separatedBy: .newlines)
                        .filter { $0.count > 0 }
 
-    var space3d = Space3D(rows)
-    space3d.cycle(6)
-    print("Part 1: \(space3d.cubes.count)")
+//    var space3d = Space3D(rows)
+//    space3d.cycle(6)
+//    print("Part 1: \(space3d.cubes.count)")
 
     var space4d = Space4D(rows)
-    let startTime = CFAbsoluteTimeGetCurrent()
-    space4d.cycle(6)
-    print("time: \(CFAbsoluteTimeGetCurrent() - startTime)")
-    print("Part 2: \(space4d.cubes.count)")
+    for n in 1...200 {
+        let startTime = CFAbsoluteTimeGetCurrent()
+        space4d.cycle(1)
+        print("cycle \(n): \(space4d.cubes.count) (\(CFAbsoluteTimeGetCurrent() - startTime))")
+    }
+//    print("Part 2: \(space4d.cubes.count)")
 
 
 }
