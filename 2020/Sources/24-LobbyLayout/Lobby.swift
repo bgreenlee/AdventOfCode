5591c2d2743668d7999bb5b1faa7d187d6c0e0fd
Brad Greenlee <brad@footle.org>
2020-12-25 11:08:45 -0800

    Day 24

--- 2020/Sources/24-LobbyLayout/Lobby.swift
+++ 2020/Sources/24-LobbyLayout/Lobby.swift
@@ -1,6 +1,8 @@
 import Foundation
 import Shared
 
+// Hexagonal Tile
+// Using a cube coordinate system (https://www.redblobgames.com/grids/hexagons/#coordinates)
 struct Tile: Hashable, CustomStringConvertible {
     var x:Int
     var y:Int
@@ -16,7 +18,7 @@ struct Tile: Hashable, CustomStringConvertible {
         self.z = z
     }
 
-    func adjacent(_ dir:String) -> Tile {
+    func neighbor(toThe dir:String) -> Tile {
         switch dir {
         case "ne": return self + ( 1, 0,-1)
         case "e":  return self + ( 1,-1, 0)
@@ -26,6 +28,17 @@ struct Tile: Hashable, CustomStringConvertible {
         case "nw": return self + ( 0, 1,-1)
         default:   return self
         }
+    }
+
+    func neighbors() -> Set<Tile> {
+        return Set([
+            self.neighbor(toThe: "ne"),
+            self.neighbor(toThe: "e"),
+            self.neighbor(toThe: "se"),
+            self.neighbor(toThe: "sw"),
+            self.neighbor(toThe: "w"),
+            self.neighbor(toThe: "nw"),
+        ])
     }
 
     static func + (lhs: Tile, rhs: (Int, Int, Int)) -> Tile {
@@ -53,13 +66,39 @@ struct Lobby {
             var tile = Tile(0,0,0)
             let dirs = re.matchGroups(in: line).flatMap({$0})
             for dir in dirs {
-                tile = tile.adjacent(dir)
+                tile = tile.neighbor(toThe: dir)
             }
             if blackTiles.contains(tile) {
                 blackTiles.remove(tile)
             } else {
                 blackTiles.insert(tile)
             }
+        }
+    }
+
+    func blackNeighbors(of tile:Tile) -> Set<Tile> {
+        return tile.neighbors().intersection(blackTiles)
+    }
+
+    func whiteNeighbors(of tile:Tile) -> Set<Tile> {
+        return tile.neighbors().subtracting(blackTiles)
+    }
+
+    mutating func cycle(_ n:Int = 1) {
+        for _ in 1...n {
+            var newTiles:Set<Tile> = []
+            for tile in blackTiles {
+                let bn = blackNeighbors(of: tile)
+                if bn.count > 0 && bn.count < 3 {
+                    newTiles.insert(tile) // remains black
+                }
+                for whiteNeighbor in whiteNeighbors(of: tile) {
+                    if blackNeighbors(of: whiteNeighbor).count == 2 {
+                        newTiles.insert(whiteNeighbor)
+                    }
+                }
+            }
+            blackTiles = newTiles
         }
     }
 }
--- 2020/Sources/24-LobbyLayout/main.swift
+++ 2020/Sources/24-LobbyLayout/main.swift
@@ -5,6 +5,9 @@ if let input = try Bundle.module.readFile("data/input.dat") {
     let lines = input.components(separatedBy: .newlines)
                        .filter { $0.count > 0 }
 
-    let lobby = Lobby(lines)
+    var lobby = Lobby(lines)
     print("Part 1: \(lobby.blackTiles.count)")
+
+    lobby.cycle(100)
+    print("Part 2: \(lobby.blackTiles.count)")
 }
