a249dad5cb601fd3bb9dd182f24814f33cbdf342
Brad Greenlee <brad@footle.org>
2021-12-03 05:39:53 -0800

    Typescript day 3 part 2

--- 2021/typescript/src/03-binary-diagnostic/part1.ts
+++ 2021/typescript/src/03-binary-diagnostic/part1.ts
@@ -1,27 +1,65 @@
 import * as fs from 'fs';
 
-let input = fs.readFileSync(process.stdin.fd, 'utf-8');
-let lines = input.split('\n');
-
-let counts = new Array<number>(lines[0].length).fill(0);
-for (let line of lines) {
-    let digits = line.split('');
-    for (let i = 0; i < digits.length; i++) {
-        counts[i] += +digits[i];
+function getGammaEpsilon(numbers: number[]): [number, number] {
+    // count total number of ones in each bit position
+    let bitSize = lines[0].length;
+    let counts = new Array<number>(bitSize).fill(0);
+    for (let num of numbers) {
+        for (let i = 0; i < bitSize; i++) {
+            // check if the corresponding bit is set
+            let mask = 1 << (bitSize - i - 1);
+            if ((num & mask) > 0) {
+                counts[i]++;
+            }
+        }
     }
+
+    let half = numbers.length / 2;
+    // most common bits are the counts that are half or more of the total number of lines
+    let gamma = counts.reduce((acc, cur, i) => {
+        if (cur >= half) {
+            acc += Math.pow(2, counts.length - i - 1); // binary -> dec
+        }
+        return acc;
+    }, 0);
+    // inverse is 0x1111..1 - gamma
+    let eplison = Math.pow(2, counts.length) - 1 - gamma;
+
+    return [gamma, eplison];
 }
 
-let halfLines = lines.length / 2;
-// most common bits are the counts that are half or more of the total number of lines
-let gamma = counts.reduce((acc, cur, i) => {
-    if (cur >= halfLines) {
-        acc += Math.pow(2, counts.length - i - 1); // binary -> dec
+let input = fs.readFileSync(process.stdin.fd, 'utf-8');
+let lines = input.split('\n');
+let numbers = lines.map((line) => parseInt(line, 2)); // binary -> dec
+
+let [gamma, epsilon] = getGammaEpsilon(numbers);
+
+console.log(`Part 1: ${gamma * epsilon}`);
+
+let bitSize = lines[0].length;
+
+let o2numbers = numbers;
+for (let i = 0; i < bitSize; i++) {
+    let [gamma, _] = getGammaEpsilon(o2numbers);
+    let mask = 1 << (bitSize - i - 1);
+    let target = gamma & mask;
+    o2numbers = o2numbers.filter((n) => (n & mask) === target);
+    if (o2numbers.length === 1) {
+        break;
     }
-    return acc;
-}, 0);
-// inverse is 0x1111..1 - gamma
-let eplison = Math.pow(2, counts.length) - 1 - gamma;
+}
+let o2generator = o2numbers[0];
 
-console.log(`Part 1: ${gamma * eplison}`);
+let co2numbers = numbers;
+for (let i = 0; i < bitSize; i++) {
+    let [_, epsilon] = getGammaEpsilon(co2numbers);
+    let mask = 1 << (bitSize - i - 1);
+    let target = epsilon & mask;
+    co2numbers = co2numbers.filter((n) => (n & mask) === target);
+    if (co2numbers.length === 1) {
+        break;
+    }
+}
+let co2scrubber = co2numbers[0];
 
-
+console.log(`Part 2: ${o2generator * co2scrubber}`);
