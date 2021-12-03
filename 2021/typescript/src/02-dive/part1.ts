455b80e71abc6b61c53c1c5f3aea190ce8e43446
Brad Greenlee <brad@footle.org>
2021-12-02 19:54:36 -0800

    Classier TS

--- 2021/typescript/src/02-dive/part1.ts
+++ 2021/typescript/src/02-dive/part1.ts
@@ -1,29 +1,38 @@
 import * as fs from 'fs';
 
+class Command {
+    movement: string;
+    distance: number;
+    readonly parseRE = /(forward|down|up)\s+(\d+)/i;
+
+    constructor(line: string) {
+        let parse = line.match(this.parseRE);
+        if (parse === null) {
+            throw new Error(`Invalid command: ${line}`);
+        }
+        this.movement = parse[1];
+        this.distance = +parse[2];
+    }
+}
+
 let input = fs.readFileSync(process.stdin.fd, 'utf-8');
 let lines = input.split('\n');
+let commands = lines.map(l => new Command(l))
 
 let pos = 0, depth = 0;
-let parseRE = /(forward|down|up)\s+(\d+)/i;
-
-for (let line of lines) {
-    let parse = line.match(parseRE);
-    if (parse !== null) {
-        let command = parse[1];
-        let distance = +parse[2];
-        switch (command) {
-            case "forward":
-                pos += distance;
-                break;
-            case "down":
-                depth += distance;
-                break;
-            case "up":
-                depth -= distance;
-            default:
-                break;
-        }
-    } 
+for (let command of commands) {
+    switch (command.movement) {
+        case "forward":
+            pos += command.distance;
+            break;
+        case "down":
+            depth += command.distance;
+            break;
+        case "up":
+            depth -= command.distance;
+        default:
+            break;
+    }
 }
 
 console.log(`pos: ${pos}, depth: ${depth}, answer: ${pos * depth}`);
--- 2021/typescript/src/02-dive/part2.ts
+++ 2021/typescript/src/02-dive/part2.ts
@@ -1,30 +1,39 @@
 import * as fs from 'fs';
 
+class Command {
+    movement: string;
+    distance: number;
+    readonly parseRE = /(forward|down|up)\s+(\d+)/i;
+
+    constructor(line: string) {
+        let parse = line.match(this.parseRE);
+        if (parse === null) {
+            throw new Error(`Invalid command: ${line}`);
+        }
+        this.movement = parse[1];
+        this.distance = +parse[2];
+    }
+}
+
 let input = fs.readFileSync(process.stdin.fd, 'utf-8');
 let lines = input.split('\n');
+let commands = lines.map(l => new Command(l))
 
 let pos = 0, depth = 0, aim = 0;
-let parseRE = /(forward|down|up)\s+(\d+)/i;
-
-for (let line of lines) {
-    let parse = line.match(parseRE);
-    if (parse !== null) {
-        let command = parse[1];
-        let distance = +parse[2];
-        switch (command) {
-            case "forward":
-                pos += distance;
-                depth += aim * distance;
-                break;
-            case "down":
-                aim += distance;
-                break;
-            case "up":
-                aim -= distance;
-            default:
-                break;
-        }
-    } 
-};
+for (let command of commands) {
+    switch (command.movement) {
+        case "forward":
+            pos += command.distance;
+            depth += aim * command.distance;
+            break;
+        case "down":
+            aim += command.distance;
+            break;
+        case "up":
+            aim -= command.distance;
+        default:
+            break;
+    }
+}
 
 console.log(`pos: ${pos}, depth: ${depth}, answer: ${pos * depth}`);
