76668910d398a76c220f55026190744470f9b296
Brad Greenlee <brad@footle.org>
2021-12-07 06:30:10 -0800

    Allow any position to be used, not just existing crab positions.

--- 2021/rust/07-the-treachery-of-whales/src/main.rs
+++ 2021/rust/07-the-treachery-of-whales/src/main.rs
@@ -4,15 +4,18 @@ fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)
         .expect("Error reading from stdin");
-    let positions: Vec<i32> = buffer.split(",")
+    let mut positions: Vec<i32> = buffer.split(",")
         .map(|n| n.trim().parse::<i32>().unwrap_or(0))
         .collect();
+    positions.sort();
+    let first = *positions.first().unwrap();
+    let last = *positions.last().unwrap();
 
     // part 1
     let mut min_score: i32 = i32::MAX;
-    for position in &positions {
+    for i in first..=last {
         let score = positions.iter()
-            .fold(0, |acc, p| acc + (p - position).abs());
+            .fold(0, |acc, p| acc + (p - i).abs());
         if score < min_score {
             min_score = score;
         }
@@ -21,10 +24,10 @@ fn main() {
 
     // part 2
     min_score = i32::MAX;
-    for position in &positions {
+    for i in first..=last {
         let score = positions.iter()
             .fold(0, |acc, p| {
-                let dist = (p - position).abs();
+                let dist = (p - i).abs();
                 return acc + dist * (dist + 1) / 2;
             });
         if score < min_score {
