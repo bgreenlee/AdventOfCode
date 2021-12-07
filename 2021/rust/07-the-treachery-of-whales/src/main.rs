3fd828ede9a919ac33a8cf8cdf0758d6a2510aad
Brad Greenlee <brad@footle.org>
2021-12-07 07:26:49 -0800

    Add median-and-mean method

--- 2021/rust/07-the-treachery-of-whales/src/main.rs
+++ 2021/rust/07-the-treachery-of-whales/src/main.rs
@@ -9,6 +9,14 @@ fn main() {
         .collect();
     positions.sort();
 
+    println!("Brute force:");
+    brute_force(&positions);
+
+    println!("\nMedian and mean:");
+    median_and_mean(&positions);
+}
+
+fn brute_force(positions: &[i32]) {
     let first = *positions.first().unwrap();
     let last = *positions.last().unwrap();
 
@@ -31,3 +39,25 @@ fn main() {
     println!("Part 2: {}", score);
 }
 
+// via https://www.reddit.com/r/adventofcode/comments/rawxad/2021_day_7_part_2_i_wrote_a_paper_on_todays/
+fn median_and_mean(positions: &[i32]) {
+    let median = positions[positions.len()/2];
+    let mean: f32 = positions.iter().sum::<i32>() as f32 / positions.len() as f32;
+
+    // part 1
+    let score = positions.iter()
+        .fold(0, |acc, p| acc + (p - median as i32).abs());
+    println!("Part 1: {}", score);
+
+    // part 2
+    let score = (0..=1).map(|i|
+        positions.iter()
+            .fold(0, |acc, p| {
+                let diff = (p - (mean + i as f32 - 0.5) as i32).abs();
+                return acc + diff * (diff + 1) / 2;
+            })
+        )
+        .min()
+        .unwrap();
+    println!("Part 2: {}", score);
+}
