1662ca0428c3591eda97f67d8d69759570030f91
Brad Greenlee <brad@footle.org>
2021-12-12 16:52:06 -0800

    Day 12, part 2

--- 2021/rust/12-passage-pathing/src/main.rs
+++ 2021/rust/12-passage-pathing/src/main.rs
@@ -20,48 +20,53 @@ impl Map {
         Map { map }
     }
 
-    fn paths(&mut self, start: &str, end: &str) -> Vec<Vec<String>> {
+    fn paths(&mut self, start: &str, end: &str, extra_visit: bool) -> Vec<Vec<String>> {
         let mut current_path: Vec<String> = Vec::new();
         let mut simple_paths: Vec<Vec<String>> = Vec::new();
-        let mut visited: HashSet<String> = HashSet::new();
 
-        current_path.push(start.to_string());
-        Self::depth_first_search(
-            self,
-            start,
-            end,
-            &mut current_path,
-            &mut simple_paths,
-            &mut visited,
-        );
-        return simple_paths;
+        Self::depth_first_search(self, start, end,
+            &mut current_path, &mut simple_paths, extra_visit);
+
+        simple_paths
     }
 
-    fn depth_first_search(
-        map: &Map,
-        start: &str,
-        end: &str,
-        current_path: &mut Vec<String>,
-        simple_paths: &mut Vec<Vec<String>>,
-        visited: &mut HashSet<String>,
-    ) {
-        let node = map.map.get(start).unwrap();
-        if node.is_small() && visited.contains(start) {
-            return;
+    fn depth_first_search(map: &Map, from: &str, to: &str,
+        current_path: &mut Vec<String>, simple_paths: &mut Vec<Vec<String>>, extra_visit: bool)
+    {
+        let node = map.map.get(from).unwrap();
+        if node.is_small() && current_path.contains(&from.to_string()) {
+            if !extra_visit || node.is_start() || node.is_end() {
+                return
+            }
+        
+            // a whole lotta work to see if we've already had our extra visit
+            let mut visit_counts: HashMap<String, u8> = HashMap::new();
+            let small_caves = current_path.into_iter()
+                .filter(|n| n.chars().nth(0).unwrap() >= 'a')
+                .map(|n| n.to_string())
+                .collect::<Vec<String>>();
+            for small_cave in small_caves {
+                *visit_counts.entry(small_cave).or_default() += 1;
+            }
+            if visit_counts.values().max().unwrap_or(&0) > &1 {
+                return;
+            }
         }
-        visited.insert(start.to_string());
-        current_path.push(start.to_string());
-        if start == end {
+
+        current_path.push(from.to_string());
+
+        if from == to {
             simple_paths.push(current_path.clone());
-            visited.remove(start);
             current_path.pop();
             return;
         }
+
         for connection in &node.connections {
-            Self::depth_first_search(map, connection, end, current_path, simple_paths, visited);
+            Self::depth_first_search(map, connection, to,
+                current_path, simple_paths, extra_visit);
         }
+
         current_path.pop();
-        visited.remove(start);
     }
 }
 
@@ -81,6 +86,14 @@ impl Node {
     fn is_small(&self) -> bool {
         self.name.chars().nth(0).unwrap() >= 'a'
     }
+
+    fn is_start(&self) -> bool {
+        self.name == "start"
+    }
+
+    fn is_end(&self) -> bool {
+        self.name == "end"
+    }
 }
 
 fn main() {
@@ -91,6 +104,9 @@ fn main() {
     let lines: Vec<&str> = buffer.lines().collect();
 
     let mut map = Map::new(lines);
-    let paths = map.paths("start", "end");
-    println!("{}", paths.len());
+    let part1_paths = map.paths("start", "end", false);
+    println!("{}", part1_paths.len());
+
+    let part2_paths = map.paths("start", "end", true);
+    println!("{}", part2_paths.len());
 }
