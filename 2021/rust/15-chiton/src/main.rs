4d7d608b8c6ae82c00070ccb5d8ce98e6d8b6d05
Brad Greenlee <brad@etsy.com>
2021-12-15 08:53:36 -0800

    Some cleanup

--- 2021/rust/15-chiton/src/main.rs
+++ 2021/rust/15-chiton/src/main.rs
@@ -1,6 +1,6 @@
-use std::io::{self, Read};
-use std::collections::{BinaryHeap, HashMap};
 use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap};
+use std::io::{self, Read};
 
 type Point = (usize, usize);
 
@@ -32,30 +32,27 @@ fn main() {
 
     // populate map
     let map = generate_map(&lines, 1);
-    let path = a_star(&map, (0,0), (map[0].len()-1, map.len()-1)).unwrap();
-    let score: u32 = path.iter().map(|(x,y)| map[*y][*x]).sum::<u32>() - map[0][0];
+    let path = a_star(&map, (0, 0), (map[0].len() - 1, map.len() - 1)).unwrap();
+    let score: u32 = path.iter().map(|(x, y)| map[*y][*x]).sum::<u32>() - map[0][0];
     println!("part 1: {}", score);
 
     let map = generate_map(&lines, 5);
-    let path = a_star(&map, (0,0), (map[0].len()-1, map.len()-1)).unwrap();
-    let score: u32 = path.iter().map(|(x,y)| map[*y][*x]).sum::<u32>() - map[0][0];
+    let path = a_star(&map, (0, 0), (map[0].len() - 1, map.len() - 1)).unwrap();
+    let score: u32 = path.iter().map(|(x, y)| map[*y][*x]).sum::<u32>() - map[0][0];
     println!("part 2: {}", score);
-
 }
 
 fn generate_map(lines: &Vec<&str>, multiplier: usize) -> Vec<Vec<u32>> {
     let height = lines.len();
     let width = lines[0].len();
     let mut map: Vec<Vec<u32>> = vec![vec![0; width * multiplier]; height * multiplier];
-    for i in 0usize..multiplier {
-        for j in 0usize..multiplier {
+    for my in 0usize..multiplier {
+        for mx in 0usize..multiplier {
             for y in 0..height {
                 for (x, c) in lines[y].char_indices() {
-                    let mut risk = c.to_digit(10).unwrap() + i as u32 + j as u32;
-                    while risk > 9 {
-                        risk -= 9;
-                    }
-                    map[height * i + y][width * j + x] = risk;
+                    let num = c.to_digit(10).unwrap();
+                    let risk = (num + my as u32 + mx as u32 - 1) % 9 + 1;
+                    map[height * my + y][width * mx + x] = risk;
                 }
             }
         }
@@ -74,10 +71,10 @@ fn reconstruct_path(came_from: &HashMap<Point, Point>, point: Point) -> Vec<Poin
     total_path
 }
 
+// heuristic for A*
+// calculates Manhattan distance
 fn heuristic(start: Point, goal: Point) -> u32 {
-    // use Manhattan distance as our estimate
-    goal.0 as u32 - start.0 as u32 +
-    goal.1 as u32 - start.1 as u32
+    (goal.0 - start.0 + goal.1 - start.1) as u32
 }
 
 fn neighbors(map: &Vec<Vec<u32>>, point: Point) -> Vec<Point> {
@@ -86,16 +83,16 @@ fn neighbors(map: &Vec<Vec<u32>>, point: Point) -> Vec<Point> {
     let width = map[0].len();
     let (x, y) = point;
     if y > 0 {
-        neighbors.push((x, y-1));
+        neighbors.push((x, y - 1));
     }
     if x < width - 1 {
-        neighbors.push((x+1, y));
+        neighbors.push((x + 1, y));
     }
     if y < height - 1 {
-        neighbors.push((x, y+1));
+        neighbors.push((x, y + 1));
     }
     if x > 0 {
-        neighbors.push((x-1, y));
+        neighbors.push((x - 1, y));
     }
     neighbors
 }
@@ -107,7 +104,10 @@ fn a_star(map: &Vec<Vec<u32>>, start: Point, goal: Point) -> Option<Vec<Point>> 
     // The set of discovered nodes that may need to be (re-)expanded.
     // Initially, only the start node is known.
     let mut open_set: BinaryHeap<Cell> = BinaryHeap::new();
-    open_set.push(Cell{location: start, fscore: map[0][0]});
+    open_set.push(Cell {
+        location: start,
+        fscore: map[0][0],
+    });
 
     // For node n, came_from[n] is the node immediately preceding it on the cheapest path from start
     // to n currently known.
@@ -123,35 +123,37 @@ fn a_star(map: &Vec<Vec<u32>>, start: Point, goal: Point) -> Option<Vec<Point>> 
     fscore.insert(start, heuristic(start, goal));
 
     while let Some(current) = open_set.pop() {
-        // println!("current: {:?}", current);
         if current.location == goal {
             return Some(reconstruct_path(&came_from, current.location));
         }
-        
         for neighbor in neighbors(map, current.location) {
-            let neighbor_risk = map[neighbor.1][neighbor.0];
-            // println!("neighbor: {:?} ({})", neighbor, neighbor_risk);
-            let tentative_gscore = *gscore.get(&current.location).unwrap_or(&max_val) + neighbor_risk;
+            // add score of current location plus the risk of the neighbor
+            let tentative_gscore =
+                *gscore.get(&current.location).unwrap_or(&max_val) + map[neighbor.1][neighbor.0];
             if tentative_gscore < *gscore.get(&neighbor).unwrap_or(&max_val) {
+                // this path is better, let's go there
                 came_from.insert(neighbor, current.location);
-                // println!("came from: {:?}", came_from);
                 gscore.insert(neighbor, tentative_gscore);
                 fscore.insert(neighbor, tentative_gscore + heuristic(neighbor, goal));
-                
-                let open_set_points: Vec<_> = open_set.clone()
+
+                let open_set_points: Vec<_> = open_set
+                    .clone()
                     .into_vec()
                     .iter()
                     .map(|cell| cell.location)
                     .collect();
-                
                 if !open_set_points.contains(&neighbor) {
-                    open_set.push(Cell{location: neighbor, fscore: *fscore.get(&neighbor).unwrap_or(&max_val)});
+                    let cell = Cell {
+                        location: neighbor,
+                        fscore: *fscore.get(&neighbor).unwrap_or(&max_val),
+                    };
+                    open_set.push(cell);
                 }
             }
         }
     }
 
-    return None
+    return None;
 }
 
 #[cfg(test)]
@@ -161,10 +163,25 @@ mod tests {
     #[test]
     fn min_heap() {
         let mut heap: BinaryHeap<Cell> = BinaryHeap::new();
-        heap.push(Cell{location: (0,0), fscore: 3});
-        heap.push(Cell{location: (1,0), fscore: 2});
-        heap.push(Cell{location: (0,1), fscore: 7});
+        heap.push(Cell {
+            location: (0, 0),
+            fscore: 3,
+        });
+        heap.push(Cell {
+            location: (1, 0),
+            fscore: 2,
+        });
+        heap.push(Cell {
+            location: (0, 1),
+            fscore: 7,
+        });
 
-        assert_eq!(heap.pop().unwrap(), Cell{location: (1,0), fscore: 2});
+        assert_eq!(
+            heap.pop().unwrap(),
+            Cell {
+                location: (1, 0),
+                fscore: 2
+            }
+        );
     }
 }
