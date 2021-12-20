880d02c1d7a90b97293097d6937b6c656aaf09c3
Brad Greenlee <brad@footle.org>
2021-12-20 07:33:50 -0800

    Day 20

---
+++ 2021/rust/20-trench-map/src/main.rs
@@ -0,0 +1,90 @@
+use std::io::{self, Read};
+use std::collections::HashSet;
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_to_string(&mut buffer)
+        .expect("Error reading from stdin");
+    let lines: Vec<&str> = buffer.lines().collect();
+
+    let algo: Vec<char> = lines[0].chars().collect();
+
+    let mut input_image: HashSet<(i32,i32)> = HashSet::new();
+    for i in 2..lines.len() {
+        let line = lines[i];
+        for (x, c) in line.char_indices() {
+            if c == '#' {
+                input_image.insert((x as i32, (i-2) as i32));
+            }
+        }
+    }
+
+    let mut background_lit = false;
+    for _ in 0..50 {
+        enhance(&mut input_image, &mut background_lit, &algo);
+    }
+    // display(&input_image);
+    println!("{}", input_image.iter().count());
+}
+
+fn enhance(image: &mut HashSet<(i32, i32)>, background_lit: &mut bool, algo: &Vec<char>) {
+    let ((minx, miny), (maxx, maxy)) = get_bounds(image);
+    let mut new_image: HashSet<(i32, i32)> = HashSet::new();
+    for y in miny-1..=maxy+1 {
+        for x in minx-1..=maxx+1 {
+            let mut algo_idx = 0;
+            for offset_y in -1..=1 {
+                for offset_x in -1..=1 {
+                    algo_idx <<= 1;
+                    let (ox, oy) = (x + offset_x, y + offset_y);
+                    // if the background is lit and we're looking outside our image, consider the pixel on
+                    if (*background_lit && (ox < minx || ox > maxx || oy < miny || oy > maxy)) ||
+                        image.contains(&(ox, oy)) {
+                        algo_idx |= 1;
+                    }
+                }
+            }
+            if algo[algo_idx] == '#' {
+                new_image.insert((x,y));
+            }
+        }
+    }
+    // if the 0 element of our algorithm is set, the background will alternate between dark and light with each iteration
+    if algo[0] == '#' {
+        *background_lit = !*background_lit;
+    }
+    *image = new_image;
+}
+
+#[allow(dead_code)]
+fn display(image: &HashSet<(i32, i32)>) {
+    let ((minx, miny), (maxx, maxy)) = get_bounds(image);
+    for y in miny..=maxy {
+        for x in minx..=maxx {
+            print!("{}", if image.contains(&(x,y)) { '#' } else { ' ' });
+        }
+        println!()
+    }
+}
+
+// get bounds of image
+// we should keep track of these separately for efficiency, but maybe later
+fn get_bounds(image: &HashSet<(i32,i32)>) -> ((i32, i32), (i32, i32)) {
+    let (mut minx, mut miny) = (i32::MAX, i32::MAX);
+    let (mut maxx, mut maxy) = (i32::MIN, i32::MIN);
+    for (x, y) in image.iter() {
+        if *x < minx { minx = *x }
+        if *x > maxx { maxx = *x }
+        if *y < miny { miny = *y }
+        if *y > maxy { maxy = *y }
+    }
+    ((minx, miny), (maxx, maxy))
+}
+
+#[allow(unused_macros)]
+macro_rules! dbg {
+    ($x:expr) => {
+        println!("{} = {:?}",stringify!($x),$x);
+    }
+}
