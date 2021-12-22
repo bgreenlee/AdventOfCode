98946efab1c10ee500de5127224a097cbaf7ae7f
Brad Greenlee <brad@etsy.com>
2021-12-21 20:49:39 -0800

    Moishe's solution

--- 2021/rust/21-dirac-dice/src/main.rs
+++ 2021/rust/21-dirac-dice/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     part1(4, 7);
-    part2(4, 8);
+    part2(4, 7);
 }
 
 fn part1(p1: u32, p2: u32) {
@@ -26,69 +26,40 @@ fn part1(p1: u32, p2: u32) {
     println!("Part 1: {}", answer);
 }
 
-fn part2(p1: u32, p2: u32) {
-    let start_pos = [p1, p2];
-
-    // number of times out of three rolls that each index comes up (3 = 1/27, 4 = 3/27, etc.)
-    let roll_outcomes = [
-        (3, 1),
-        (4, 3),
-        (5, 6),
-        (6, 7),
-        (7, 6),
-        (8, 3),
-        (9, 1),
-    ];
-
-    // positions & outcomes for each round, per player
-    let mut rounds: [Vec<Vec<(u32,u32,u64)>>; 2] = [Vec::new(), Vec::new()];
-
-    // pre-populate the first round
-    // println!("round 1:");
-    for player in 0..2 {
-        let mut pos_scores: Vec<(u32,u32,u64)> = Vec::new();
-        // println!("  player {}:", player+1);
-        for (roll, outcomes) in roll_outcomes.into_iter() {
-            let pos = (start_pos[player] + roll - 1) % 10 + 1;
-            pos_scores.push((pos, pos, outcomes));
-            // println!("    ({}, {}, {})", pos, pos, outcomes);
-        }
-        rounds[player].push(pos_scores);
-    }
-
-    // do the remaining rounds; after each roll we check for a winner
+fn part2(p1: u8, p2: u8) {
     let mut wins: [u64; 2] = [0, 0];
-    for round in 1..10 {
-        // println!("round {}:", round+1);
-        for player in 0..2 {
-            if &rounds[player].len() < &round {
-                break; // player is done
-            }
-            // println!("  player {}:", player+1);
-            // for each of the outcomes of the last round, run through all the possible outcomes for this round
-            let mut pos_scores: Vec<(u32,u32,u64)> = Vec::new();
-            for (last_pos, last_score, last_outcomes) in &rounds[player][round-1] {
-                for (roll, outcomes) in roll_outcomes.into_iter() {
-                    let pos = (last_pos + roll - 1) % 10 + 1;
-                    let score = last_score + pos;
-                    let num_outcomes = last_outcomes * outcomes;
-                    if score >= 21 {
-                        wins[player] += num_outcomes;
-                    } else {
-                        // println!("    ({}, {}, {})", pos, score, num_outcomes);
-                        pos_scores.push((pos, score, num_outcomes));
-                    }
-                }
-            }
-            println!("round {} player {} scores {}", round, player + 1, pos_scores.len());
-            if !pos_scores.is_empty() {
-                rounds[player].push(pos_scores);
-            }
-        }
-        println!("Round {}: p1 = {}, p2 = {}", round, wins[0], wins[1]);
-    }
+    split_universe(&mut wins, 0, [p1,p2], [0,0], 1);
+    println!("Part 2:");
+    println!("  Player 1: {}", wins[0]);
+    println!("  Player 2: {}", wins[1]);
 }
 
+// cribbed from Moishe
+// https://github.com/Moishe/aoc-21/blob/main/day-21/part-2.py
+fn split_universe(wins: &mut [u64; 2], player: usize, positions: [u8; 2], scores: [u8; 2], instances: u64) {
+    // number of times out of three rolls that each index comes up (3 = 1/27, 4 = 3/27, etc.)
+    static ROLL_OUTCOMES: [u8; 10] = [0, 0, 0, 1, 3, 6, 7, 6, 3, 1];
+
+    for roll in 3..=9 {
+        let new_positions;
+        let new_scores;
+        let new_pos = (positions[player] + roll - 1) % 10 + 1;
+        let new_score = scores[player] + new_pos;
+        if player == 0 {
+            new_positions = [new_pos, positions[1]];
+            new_scores = [new_score, scores[1]];
+        } else {
+            new_positions = [positions[0], new_pos];
+            new_scores = [scores[0], new_score];
+        }
+
+        if new_scores[player] >= 21 {
+            wins[player] += instances * ROLL_OUTCOMES[roll as usize] as u64;
+        } else {
+            split_universe(wins, (player + 1) % 2, new_positions, new_scores, instances * ROLL_OUTCOMES[roll as usize] as u64);
+        }
+    }
+}
 
 #[allow(unused_macros)]
 macro_rules! dbg {
