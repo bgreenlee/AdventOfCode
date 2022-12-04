130837966591898c0872949b252226ec0a5f2fdc
Brad Greenlee <brad@footle.org>
2022-12-04 05:11:38 -0800

    Use Filter instead of looping & counting

--- 2022/04-camp-cleanup/main.go
+++ 2022/04-camp-cleanup/main.go
@@ -10,19 +10,16 @@ import (
 )
 
 func countOverlaps(lines []string, comparisonFn func(n []int) bool) int {
-	count := 0
 	sep := regexp.MustCompile("[,-]")
-	for _, line := range lines {
+	// filter down to lines that satisfy the comparison function
+	return len(util.Filter(lines, func(line string) bool {
 		// convert line to an array of ints "2-4,6-8" -> [2,4,6,8]
 		n := util.Map(sep.Split(line, -1), func(s string) int {
 			i, _ := strconv.Atoi(s)
 			return i
 		})
-		if comparisonFn(n) {
-			count += 1
-		}
-	}
-	return count
+		return comparisonFn(n)
+	}))
 }
 
 // ranges in which one completely covers the other
