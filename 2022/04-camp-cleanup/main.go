fc5055a93a1315495da48284760f022bdeb27959
Brad Greenlee <brad@footle.org>
2022-12-04 04:59:43 -0800

    More cleanup

--- 2022/04-camp-cleanup/main.go
+++ 2022/04-camp-cleanup/main.go
@@ -9,8 +9,7 @@ import (
 	"github.com/bgreenlee/AdventOfCode/tree/main/2022/util"
 )
 
-// count number of ranges that are completely contained by the other range
-func part1(lines []string) int {
+func countOverlaps(lines []string, comparisonFn func(n []int) bool) int {
 	count := 0
 	sep := regexp.MustCompile("[,-]")
 	for _, line := range lines {
@@ -19,33 +18,26 @@ func part1(lines []string) int {
 			i, _ := strconv.Atoi(s)
 			return i
 		})
-		if (n[0] <= n[2] && n[1] >= n[3]) || (n[2] <= n[0] && n[3] >= n[1]) {
+		if comparisonFn(n) {
 			count += 1
 		}
 	}
 	return count
 }
 
-// count number of ranges that overlap
-func part2(lines []string) int {
-	count := 0
-	sep := regexp.MustCompile("[,-]")
-	for _, line := range lines {
-		// convert line to an array of ints "2-4,6-8" -> [2,4,6,8]
-		n := util.Map(sep.Split(line, -1), func(s string) int {
-			i, _ := strconv.Atoi(s)
-			return i
-		})
-		if (n[1] >= n[2] && n[0] <= n[3]) || (n[3] >= n[0] && n[2] <= n[1]) {
-			count += 1
-		}
-	}
-	return count
+// ranges in which one completely covers the other
+func overlapComplete(n []int) bool {
+	return (n[0] <= n[2] && n[1] >= n[3]) || (n[2] <= n[0] && n[3] >= n[1])
+}
+
+// ranges that overlap partially
+func overlapPartial(n []int) bool {
+	return (n[1] >= n[2] && n[0] <= n[3]) || (n[3] >= n[0] && n[2] <= n[1])
 }
 
 func main() {
 	lines := util.ReadLines(os.Stdin)
 
-	fmt.Printf("Part 1: %d\n", part1(lines))
-	fmt.Printf("Part 2: %d\n", part2(lines))
+	fmt.Printf("Part 1: %d\n", countOverlaps(lines, overlapComplete))
+	fmt.Printf("Part 2: %d\n", countOverlaps(lines, overlapPartial))
 }
