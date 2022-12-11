8b1503106dbfe57276798d4d946cd02467e280cd
Brad Greenlee <brad@footle.org>
2022-12-11 06:06:41 -0800

    Day 11, part 1

---
+++ 2022/11-monkey-in-the-middle/main.py
@@ -0,0 +1,66 @@
+import sys
+import re
+from dataclasses import dataclass
+from collections import deque
+
+@dataclass
+class Monkey:
+    id: int
+    items: deque[int]
+    op: str
+    operand: str
+    test: int
+    if_true: int
+    if_false: int
+    inspect_count: int = 0
+
+    def inspect_item(self, item: int) -> tuple[int, int]:
+        self.inspect_count += 1
+        scalar = item if self.operand == "old" else int(self.operand)
+        worry_level = 0
+        match self.op:
+            case '+':
+                worry_level = item + scalar
+            case '*':
+                worry_level = item * scalar
+
+        worry_level = int(worry_level / 3)
+        if worry_level % self.test == 0:
+            return worry_level, self.if_true
+        else:
+            return worry_level, self.if_false
+
+
+monkies: list[Monkey] = []
+
+input = sys.stdin.read()
+monkey_re = re.compile(r"""
+        Monkey\ (?P<id>\d+):\n
+        \s+Starting\ items:\ (?P<items>[\d, ]+)\n
+        \s+Operation:\ new\ =\ old\ (?P<op>.)\ (?P<operand>\w+)\n
+        \s+Test:\ divisible\ by\ (?P<test>\d+)\n
+        \s+If\ true:\ throw\ to\ monkey\ (?P<if_true>\d+)\n
+        \s+If\ false:\ throw\ to\ monkey\ (?P<if_false>\d+)""", re.X)
+
+for m in monkey_re.finditer(input):
+    monkey = Monkey(id=int(m.group('id')),
+                    items=deque([int(i) for i in m.group('items').split(', ')]),
+                    op=m.group('op'),
+                    operand=m.group('operand'),
+                    test=int(m.group('test')),
+                    if_true=int(m.group('if_true')),
+                    if_false=int(m.group('if_false')))
+    monkies.append(monkey)
+
+for round in range(20):
+    for monkey in monkies:
+        while monkey.items:
+            item = monkey.items.popleft()
+            new_item, dest = monkey.inspect_item(item)
+            # print(f"Monkey {monkey.id}, item {item}: throw to {dest}")
+            monkies[dest].items.append(new_item)
+
+
+monkies.sort(key=lambda m: m.inspect_count, reverse=True)
+# print(monkies)
+print(monkies[0].inspect_count * monkies[1].inspect_count)
