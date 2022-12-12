ae3e466794c0b2c49f8555080550aa42d5610170
Brad Greenlee <brad@etsy.com>
2022-12-12 09:02:55 -0800

    Move parsing to its own function

--- 2022/12-hill-climbing-algorithm/main.py
+++ 2022/12-hill-climbing-algorithm/main.py
@@ -58,30 +58,34 @@ def dijkstra(grid: Grid, start: Point, end: Point) -> tuple[dict, dict]:
 
     return parents, dists    
 
+
+def parse_input(lines: list[str]) -> tuple[Grid, Point, Point]:
+    grid: Grid = {}
+    start: Point
+    end: Point
+    for y in range(len(lines)):
+        for x in range(len(lines[y])):
+            match lines[y][x]:
+                case 'S':
+                    start = (x, y)
+                    grid[start] = ord('a')
+                case 'E':
+                    end = (x, y)
+                    grid[end] = ord('z')
+                case c:
+                    grid[(x, y)] = ord(c)
+
 #
 # main
 #
-grid: Grid = {}
-start: Point
-end: Point
-lines = [line.rstrip() for line in sys.stdin]
-for y in range(len(lines)):
-    for x in range(len(lines[y])):
-        match lines[y][x]:
-            case 'S':
-                start = (x, y)
-                grid[start] = ord('a')
-            case 'E':
-                end = (x, y)
-                grid[end] = ord('z')
-            case c:
-                grid[(x, y)] = ord(c)
 
+grid, start, end = parse_input([line.rstrip() for line in sys.stdin])
+
+# part1: find shortest path from start to end
 path = find_path(grid, start, end)
 print("Part 1:", len(path) - 1) # path includes start, so subtract one
 
-# find paths for all lowest points
+# part 2: find the shortest path starting from all the lowest points
 low_points = [point for point in grid if grid[point] == ord('a')]
-# find the shortest, ignoring any empty paths (since that indicates it couldn't get from start to end)
 shortest = min([len(p) for p in [find_path(grid, lp, end) for lp in low_points] if len(p) > 0])
 print("Part 2:", shortest - 1)
