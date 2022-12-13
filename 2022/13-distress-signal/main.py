01eb58c5c4369ab449dd188869db5a115076481c
Brad Greenlee <brad@etsy.com>
2022-12-13 06:49:48 -0800

    Comments and cleanup

--- 2022/13-distress-signal/main.py
+++ 2022/13-distress-signal/main.py
@@ -1,32 +1,36 @@
 import sys
 from functools import cmp_to_key
 
-def packet_order(left: list|int, right: list|int) -> int:
-    match left, right:
+# sort function for packets
+# returns -1 if left < right, 1 if left > right, 0 if equal
+def packet_order(a: list|int, b: list|int) -> int:
+    match a, b:
         case list(), list():
-            for i, a in enumerate(left):
-                if i >= len(right):
-                    return 1
-                b = right[i]
-                if (ordered := packet_order(a, b)) != 0:
-                    return ordered
+            for i in range(len(a)):
+                if i >= len(b):
+                    return 1 # no more right
+                if (order := packet_order(a[i], b[i])) != 0:
+                    return order
             return -1
         case list(), int():
-            return packet_order(left, [right])
+            return packet_order(a, [b])
         case int(), list():
-            return packet_order([left], right)
+            return packet_order([a], b)
         case int(), int():
-            return (left > right) - (left < right) # cmp
+            return (a > b) - (a < b) # cmp
+
 
 def part1(packets: list) -> int:
     ordered = [packet_order(a, b) for a, b in zip(packets[::2], packets[1::2])]
-    return sum([i+1 for i, b in enumerate(ordered) if b == -1])
+    return sum([i+1 for i, o in enumerate(ordered) if o == -1])
+
 
 def part2(packets: list) -> int:
     dividers = [[[2]],[[6]]]
     packets.extend(dividers)
     packets.sort(key=cmp_to_key(packet_order))
     return (packets.index(dividers[0]) +1 ) * (packets.index(dividers[1]) + 1)
+
 
 #
 # main
