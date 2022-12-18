fd08dd60cc3ead981d0973684fa38fd8f984fbba
Brad Greenlee <brad@footle.org>
2022-12-18 07:13:47 -0800

    More cleanup

--- 2022/18-boiling-boulders/main.py
+++ 2022/18-boiling-boulders/main.py
@@ -3,17 +3,22 @@ from collections import deque
 
 Point = tuple[int, int, int]
 
-def neighbors(points: set[Point], point: Point) -> list[Point]:
-    x, y, z = point
-    return [p for p in [(x + dx, y + dy, z + dz)
-            for dx, dy, dz in [(0,0,1), (0,1,0), (1,0,0), (0,0,-1), (0,-1,0), (-1,0,0)]]
-            if p in points]
+# adjacent coordinates
+def adjacent(p: Point) -> list[Point]:
+    return [(p[0] + dx, p[1] + dy, p[2] + dz) for dx, dy, dz in [(0,0,1), (0,1,0), (1,0,0), (0,0,-1), (0,-1,0), (-1,0,0)]]
 
+# populated adjacent points
+def neighbors(points: set[Point], point: Point) -> list[Point]:
+    return [p for p in adjacent(point) if p in points]
+
+# count all open surfaces
 def part1(points: set[Point]) -> int:
     return len(points) * 6 - sum(len(neighbors(points, point)) for point in points)
 
-# create a bounding box around the points and flood fill the interior
+# count all exterior surfaces
 def part2(points: set[Point]) -> int:
+    # create a bounding box around the points and flood fill the interior
+
     # bounding box
     x_min, x_max = min(p[0] for p in points) - 1, max(p[0] for p in points) + 1
     y_min, y_max = min(p[1] for p in points) - 1, max(p[1] for p in points) + 1
@@ -26,7 +31,7 @@ def part2(points: set[Point]) -> int:
         x, y, z = p = queue.popleft()
         if p not in points and p not in flooded and x_min <= x <= x_max and y_min <= y <= y_max and z_min <= z <= z_max:
             flooded.add(p)
-            queue.extend((x + dx, y + dy, z + dz) for dx, dy, dz in [(0,0,1), (0,1,0), (1,0,0), (0,0,-1), (0,-1,0), (-1,0,0)])
+            queue.extend(adjacent(p))
 
     return sum(len(neighbors(points, point)) for point in flooded)
 
