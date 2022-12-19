c9bc507689920f8788454636f134b14f4b37894a
Brad Greenlee <brad@etsy.com>
2022-12-19 06:06:59 -0800

    Day 16, part 2

--- 2022/16-proboscidea-volcanium/main.py
+++ 2022/16-proboscidea-volcanium/main.py
@@ -1,10 +1,10 @@
 import sys
 import re
-import heapq
-from collections import defaultdict, deque
+from collections import deque
 from typing import NamedTuple
 from numpy import array, zeros, ndarray
-
+from itertools import combinations
+from functools import cache
 
 class Valve(NamedTuple):
     name: str
@@ -21,6 +21,7 @@ def parse_input(lines: list[str]) -> dict[str, Valve]:
             valves[name] = Valve(name, flow, neighbors)
 
     return valves
+
 
 # Seidel's algorithm for shorted path between all pairs in a graph
 # adapted from https://en.wikipedia.org/wiki/Seidel%27s_algorithm and https://stackoverflow.com/a/67438259
@@ -57,63 +58,65 @@ def adj_matrix(valves: dict[str, Valve]) -> ndarray:
     return m
 
 
-def part1(valves: dict[str, Valve]) -> int:
-    apd = all_pairs_distance(adj_matrix(valves))
-    nodes = sorted(valves.keys())
-    unopened = {name for name, valve in valves.items() if valve.rate > 0}
-    time_left = 30
-    paths = []
-    path = []
+@cache
+def distance(a: str, b: str) -> int:
+    return apd[nodes.index(a), nodes.index(b)]
+
+
+def max_flow(unopened: set[str], time_left: int) -> int:
+    max_flows = []
     rate = 0
     flow = 0
 
-    queue: deque[tuple[str, set, list, int, int, int]] = deque(
-        [('AA', unopened, path, time_left, rate, flow)]
+    queue: deque[tuple[str, set, int, int, int]] = deque(
+        [('AA', unopened, time_left, rate, flow)]
     )
     while len(queue) > 0:
-        node, unopened, path, time_left, rate, flow  = queue.popleft()
+        node, unopened, time_left, rate, flow  = queue.popleft()
 
-        if len(unopened) == 0:
-            # all done, just ride it out
-            paths.append((flow, path))
+        if len(unopened) == 0: # all done, just ride it out
+            max_flows.append(flow)
             continue
 
         for v in unopened:
-            dist = apd[nodes.index(node), nodes.index(v)]
+            dist = distance(node, v)
             if dist >= time_left: # can't reach it in time
-                paths.append((flow, path))
+                max_flows.append(flow)
                 continue
             # move to v and open it
             new_time_left = time_left - dist - 1
-            new_rate = rate + valves[v].rate
-            new_flow = flow + valves[v].rate * new_time_left
-            queue.append((v, unopened - {v}, path + [v], new_time_left, new_rate, new_flow))
+            vrate = valves[v].rate
+            queue.append((v, unopened - {v}, new_time_left, rate + vrate, flow + vrate * new_time_left))
 
-    paths.sort(reverse=True)
-    print(paths[0])
-    return paths[0][0]
+    return max(max_flows)
+
+
+def part1() -> int:
+    unopened = {name for name, valve in valves.items() if valve.rate > 0}
+    return max_flow(unopened, 30)
+
+def part2() -> int:
+    unopened = {name for name, valve in valves.items() if valve.rate > 0}
+    max_max_flow = 0
+
+    # partition unopened
+    for combo in combinations(unopened, int(len(unopened)/2)):
+        f1 = max_flow(set(combo), 26)
+        f2 = max_flow(unopened - set(combo), 26)
+        max_max_flow = max(max_max_flow, f1 + f2)
+
+    return max_max_flow
 
 #
 # main
 #
 
-valves = dict[str, Valve]()
 lines = [line.rstrip() for line in sys.stdin]
-# lines = [
-#     "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB",
-#     "Valve BB has flow rate=13; tunnels lead to valves CC, AA",
-#     "Valve CC has flow rate=2; tunnels lead to valves DD, BB",
-#     "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE",
-#     "Valve EE has flow rate=3; tunnels lead to valves FF, DD",
-#     "Valve FF has flow rate=0; tunnels lead to valves EE, GG",
-#     "Valve GG has flow rate=0; tunnels lead to valves FF, HH",
-#     "Valve HH has flow rate=22; tunnel leads to valve GG",
-#     "Valve II has flow rate=0; tunnels lead to valves AA, JJ",
-#     "Valve JJ has flow rate=21; tunnel leads to valve II",
-# ]
 
+global valves, apd, nodes
 valves = parse_input(lines)
+apd = all_pairs_distance(adj_matrix(valves))
+nodes = sorted(valves.keys())
 
-flow = part1(valves)
-print("Part 1:", flow)
-
+print("Part 1:", part1())
+print("Part 2:", part2())
