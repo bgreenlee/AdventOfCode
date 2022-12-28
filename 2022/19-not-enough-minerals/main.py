aabcdd2b706d8a4438e6f7fa0b30dc48b2360e36
Brad Greenlee <brad@footle.org>
2022-12-27 18:07:24 -1000

    Day 19

--- 2022/19-not-enough-minerals/main.py
+++ 2022/19-not-enough-minerals/main.py
@@ -1,66 +1,82 @@
 import sys
 import re
 import math
-from pprint import pp
 
-def solve_blueprint(bp: tuple[int, ...]) -> int:
-    robot_cost = {
-        "ore":      { "ore": bp[0], "clay": 0,     "obsidian": 0 },
-        "clay":     { "ore": bp[1], "clay": 0,     "obsidian": 0 },
-        "obsidian": { "ore": bp[2], "clay": bp[3], "obsidian": 0 },
-        "geode":    { "ore": bp[4], "clay": 0,     "obsidian": bp[5] },
-    }
-    resources: dict[str, int] = { "ore": 0, "clay": 0, "obsidian": 0, "geode": 0 }
-    robots: dict[str, int] = { "ore": 1, "clay": 0, "obsidian": 0, "geode": 0 }
+# I couldn't get my version working, so I borrowed heavily from
+# https://github.com/betaveros/advent-of-code-2022/blob/main/p19.noul
+# in order to move on with my life.
 
-    time_left = 24
-    minute = 0
-    build_robot = None # which robot to build this round
-    while (minute := minute + 1) <= time_left:
-        print(f"\n== Minute {minute} ==")
+ORE, CLAY, OBSIDIAN, GEODE = range(4)
 
-        # decide whether to build a robot or do nothing
-        possible_robots = []
-        for robot, cost in robot_cost.items():
-            if all(num <= resources[resource] for resource, num in cost.items()):
-                possible_robots.append(robot)
-        print("Can build:", possible_robots)
+# DFS with a lot of optimizations
+def solve_blueprint(bp: tuple[int, ...], time_left: int) -> int:
+    robot_cost = [
+        (bp[0], 0, 0, 0), # orebot: ore, clay, obsidian, geode
+        (bp[1], 0, 0, 0), # claybot
+        (bp[2], bp[3], 0, 0), # obsidianbot
+        (bp[4], 0, bp[5], 0), # geodebot
+    ]
+    resources: tuple[int,...] = (0, 0, 0, 0) # ore, clay, obsidian, geode
+    robots: tuple[int,...] = (1, 0, 0, 0) # ore, clay, obsidian, geode
+    max_ore_cost = max(cost[ORE] for cost in robot_cost) # most ore we need to build anything
+    best = 0 # our best geode score
 
-        # choose the best one
-        for robot in ["geode", "obsidian", "clay", "ore"]:
-            if robot in possible_robots:
-                build_robot = robot
-                # remove the resources
-                for resource, num in robot_cost[build_robot].items():
-                    resources[resource] -= num
-                print("Building:", robot)
-                break
+    stack: list[tuple[tuple[int,...], tuple[int,...], int]] = [(resources, robots, time_left)]
+    while stack:
+        resources, robots, time_left = stack.pop()
 
-        # update resources
-        for robot, num in robots.items():
-            resources[robot] += num
-            if num > 0:
-                print(f"{num} {robot} robots; now have {resources[robot]} {robot}")
+        best_idle = resources[GEODE] + robots[GEODE] * time_left # how many geodes we'd have if we did nothing else
+        if best_idle > best:
+            best = best_idle
 
-        # finish building the robot
-        if build_robot:
-            robots[build_robot] += 1
-            print(f"The new {build_robot} robot is ready; you now have {robots[build_robot]} of them.")
-            build_robot = None
+        best_max = best_idle + time_left * (time_left - 1) // 2 # most we could expect to get if we produced a geode bot every minute from here on out
+        if best_max <= best:
+            continue # can't beat the current best, so bail on this path
 
-    return resources["geode"]
+        for robot, cost in enumerate(robot_cost):
+            if robot == ORE and robots[ORE] >= max_ore_cost:
+                continue # we have enough ore
+            if robot == CLAY and robots[CLAY] >= robot_cost[OBSIDIAN][CLAY]:
+                continue # we have enough clay
+            if robot == OBSIDIAN and robots[OBSIDIAN] >= robot_cost[GEODE][OBSIDIAN]:
+                continue # we have enough obsidian
+
+            # calculate how many minutes we can jump forward
+            minutes: list[int] = []
+            for num_resource, num_robots, resource_cost in zip(resources, robots, cost):
+                if num_resource >= resource_cost:
+                    minutes.append(0) # we have enough to build a new robot
+                elif num_robots > 0:
+                    minutes.append((resource_cost - num_resource + num_robots - 1) // num_robots) # wait until we have enough
+                else:
+                    minutes.append(999) # no robot of this type and not enough resources
+            min_to_jump = max(minutes)
+
+            if min_to_jump < 999 and min_to_jump < time_left:
+                new_resources = [resources[i] + (min_to_jump + 1) * robots[i] - cost[i] for i in range(4)]
+                new_robots = [n + 1 if i == robot else n for i, n in enumerate(robots)] # build robot
+                stack.append((tuple(new_resources), tuple(new_robots), time_left - min_to_jump - 1))
+
+    return best
 
 #
 # main
 #
+if __name__ == "__main__":
+    lines = sys.stdin.read().splitlines()
+    blueprints = []
+    for line in lines:
+        nums = [int(n) for n in re.split(r'\D+', line) if n.isnumeric()]
+        blueprints.append(tuple(nums[1:]))
 
-lines = open('test.dat').read().splitlines()
-blueprints = []
-for line in lines:
-    nums = [int(n) for n in re.split(r'\D+', line) if n.isnumeric()]
-    blueprints.append(tuple(nums[1:]))
+    # part 1
+    results = [solve_blueprint(bp, 24) for bp in blueprints]
+    quality_levels = [result * (i+1) for i, result in enumerate(results)]
+    print("Part 1:", sum(quality_levels))
 
-# for bp in blueprints:
-solve_blueprint(blueprints[0])
+    # part 2
+    results = [solve_blueprint(bp, 32) for bp in blueprints[:3]]
+    print("Part 2:", math.prod(results))
+
 
 
