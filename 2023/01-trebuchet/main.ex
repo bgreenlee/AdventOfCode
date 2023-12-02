e9d136b18a74b750d8148e77663b0ebadf93a750
Brad Greenlee <brad@footle.org>
2023-12-02 03:49:52 -0800

    More simplifying

--- 2023/01-trebuchet/main.ex
+++ 2023/01-trebuchet/main.ex
@@ -1,37 +1,20 @@
 defmodule Main do
-  def line_to_number(line) do
-    line
-    # remove non-digits
-    |> String.replace(~r/[^\d]/, "")
-    # remove all but first and last digit
-    |> String.replace(~r/(^\d).*?(\d$)/, "\\1\\2")
-    # single digits get doubled, ignore no digits
-    |> (fn n ->
-          case String.length(n) do
-            0 -> "0"
-            1 -> "#{n}#{n}"
-            _ -> n
-          end
-        end).()
-    |> String.to_integer()
-  end
-
-  # recursively replace words with numbers until no more words are found
-  def words_to_numbers(line) do
+  def line_to_number(line, convert_words \\ false) do
     number_regex = ~r/^(one|two|three|four|five|six|seven|eight|nine)/
     word_map = %{"one" => "1", "two" => "2", "three" => "3", "four" => "4", "five" => "5", "six" => "6", "seven" => "7", "eight" => "8", "nine" => "9"}
     line
       |> String.graphemes()
       |> Enum.with_index()
       |> Enum.map(fn {char, idx} ->
-          rest_of_line = String.slice(line, idx..-1)
-          if String.match?(rest_of_line, number_regex) do
-            String.replace(rest_of_line, number_regex, fn word -> word_map[word] end)
-          else
-            char
+          cond do
+            String.match?(char, ~r/\d/) -> char
+            convert_words -> if match = Regex.run(number_regex, String.slice(line, idx..-1)), do: word_map[List.last(match)]
+            true -> ""
           end
         end)
       |> Enum.join()
+      |> (fn n -> String.slice(n, 0, 1) <> String.slice(n, -1, 1) end).()
+      |> String.to_integer()
   end
 
   def part1(lines) do
@@ -42,8 +25,7 @@ defmodule Main do
 
   def part2(lines) do
     lines
-    |> Enum.map(&words_to_numbers/1)
-    |> Enum.map(&line_to_number/1)
+    |> Enum.map(&line_to_number(&1, true))
     |> Enum.sum()
   end
 end
