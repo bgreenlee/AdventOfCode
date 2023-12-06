c9fee9497bcacaaf176ff4ffe5ecd3a1ae1261a5
Brad Greenlee <brad@etsy.com>
2023-12-06 05:03:33 -0800

    Add closed-form solution

--- 2023/06-wait-for-it/main.exs
+++ 2023/06-wait-for-it/main.exs
@@ -26,8 +26,11 @@ defmodule Main do
   defp solve_races(races) do
     races
     |> Enum.map(fn {time, record} ->
-      Enum.map(1..(time - 1), fn t -> (time - t) * t end)
-      |> Enum.count(fn x -> x > record end)
+      # original iterative solution
+      # Enum.map(1..(time - 1), fn t -> (time - t) * t end) |> Enum.count(fn x -> x > record end)
+      # ChatGPT's closed-form solution (https://chat.openai.com/share/d9cb1265-ee46-4e46-bbb8-2ba74361d24b)
+      x = :math.sqrt(:math.pow(time, 2) - 4 * record)/2
+      trunc(1 - ceil(time / 2 - x) + floor(time / 2 + x))
     end)
     |> Enum.product()
   end
