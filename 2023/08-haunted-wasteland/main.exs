144953443da3b19a2fead8c7efbf59ef964927d6
Brad Greenlee <brad@etsy.com>
2023-12-08 08:57:57 -0800

    Speed things up by not using Enum.at

--- 2023/08-haunted-wasteland/main.exs
+++ 2023/08-haunted-wasteland/main.exs
@@ -1,13 +1,14 @@
 defmodule Main do
   # recursively find the first node matching the end node pattern,
   # returning the number of steps to get there
-  def count_steps(nodes, current_node, end_node_pattern, steps, step_num) do
-    step = Enum.at(steps, Integer.mod(step_num, length(steps)))
+  def count_steps(nodes, current_node, end_node_pattern, original_steps, remaining_steps, step_num) do
+    [step | tail] = remaining_steps
+    remaining_steps = if tail == [], do: original_steps, else: tail # reset if we ran out
     target_node = nodes[current_node][step]
     if String.match?(target_node, end_node_pattern) do
       step_num + 1
     else
-      count_steps(nodes, target_node, end_node_pattern, steps, step_num + 1)
+      count_steps(nodes, target_node, end_node_pattern, original_steps, remaining_steps, step_num + 1)
     end
   end
 
@@ -22,14 +23,14 @@ defmodule Main do
   end
 
   def part1(steps, nodes) do
-    count_steps(nodes, "AAA", ~r/^ZZZ$/, steps, 0)
+    count_steps(nodes, "AAA", ~r/^ZZZ$/, steps, steps, 0)
   end
 
   def part2(steps, nodes) do
     # get all starting nodes
     Enum.filter(Map.keys(nodes), &String.match?(&1, ~r/A$/))
     # find cycle length for each start node
-    |> Enum.map(fn node -> count_steps(nodes, node, ~r/Z$/, steps, 0) end)
+    |> Enum.map(fn node -> count_steps(nodes, node, ~r/Z$/, steps, steps, 0) end)
     # calculate least common multiple of all cycles
     |> Enum.reduce(fn n, acc -> div(acc * n, Integer.gcd(acc, n)) end)
   end
