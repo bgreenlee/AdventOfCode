ed4f64d08eb0396e7b3599632c8fbd9be14e76cf
Brad Greenlee <brad@footle.org>
2024-12-04 09:57:45 -0800

    tweaks and formatting

--- 2024/AdventOfCode/Solutions/Day 4/CeresSearch.swift
+++ 2024/AdventOfCode/Solutions/Day 4/CeresSearch.swift
@@ -6,17 +6,12 @@
 //
 
 typealias point = SIMD2<Int>
-typealias vec = SIMD2<Int> // pendantry
+typealias vec = SIMD2<Int>  // pendantry
 
 struct Grid: Sequence {
     private var grid: [point: Character] = [:]
-    let width: Int
-    let height: Int
 
-    init (_ rows: [String]) {
-        self.width = rows.isEmpty ? 0 : rows[0].count
-        self.height = rows.count
-
+    init(_ rows: [String]) {
         for y in 0..<rows.count {
             let chars = Array(rows[y])
             for x in 0..<chars.count {
@@ -35,20 +30,28 @@ struct Grid: Sequence {
 
     // get all words in each of the cardinal directions
     func words(at p: point, length: Int = 4) -> [String] {
-        var words: [String] = []
-        for v in [vec(0,-1), vec(1,-1), vec(1,0), vec(1,1), vec(0,1), vec(-1,1), vec(-1,0), vec(-1,-1)] {
-            words.append(String((0..<length).map { at(point(p.x, p.y) &+ v &* $0) }))
+        [
+            vec(0, -1), vec(1, -1), vec(1, 0), vec(1, 1),
+            vec(0, 1), vec(-1, 1), vec(-1, 0), vec(-1, -1),
+        ].map { v in
+            String(
+                (0..<length).map {
+                    at(point(p.x, p.y) &+ v &* $0)
+                }
+            )
         }
-        return words
     }
 
     // get the two 3-letter words that make up the X
     func xwords(at p: point) -> [String] {
         [
-            String([at(point(p.x - 1, p.y + 1)), at(point(p.x, p.y)), at(point(p.x + 1, p.y - 1))]),
-            String([at(point(p.x - 1, p.y - 1)), at(point(p.x, p.y)), at(point(p.x + 1, p.y + 1))]),
+            String([
+                at(point(p.x - 1, p.y + 1)), at(point(p.x, p.y)), at(point(p.x + 1, p.y - 1)),
+            ]),
+            String([
+                at(point(p.x - 1, p.y - 1)), at(point(p.x, p.y)), at(point(p.x + 1, p.y + 1)),
+            ]),
         ]
-
     }
 }
 class CeresSearch: Solution {
