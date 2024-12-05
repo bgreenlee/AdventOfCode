9b63bfec423f6596d4aee4d66412a2d495c67e68
Brad Greenlee <brad@footle.org>
2024-12-05 08:56:49 -0800

    Much faster version

--- 2024/AdventOfCode/Solutions/Day 5/PrintQueue.swift
+++ 2024/AdventOfCode/Solutions/Day 5/PrintQueue.swift
@@ -5,31 +5,23 @@
 //  Created by Brad Greenlee on 12/5/24.
 //
 
+typealias Rule = SIMD2<Int>
+
 class PrintQueue: Solution {
     init() {
         super.init(id: 5, name: "Print Queue")
     }
 
-    func parseInput(_ input: [String]) -> ([[Int]], [[Int]]) {
+    func parseInput(_ input: [String]) -> (Set<Rule>, [[Int]]) {
         let splitIndex = input.firstIndex(of: "")!
         let rulesInput = input[..<splitIndex]
         let updatesInput = input[splitIndex.advanced(by: 1)...]
-        let rules = rulesInput.map { $0.split(separator: "|").map { Int($0)! }}
+        let rules = Set(rulesInput.map {
+            let parts = $0.split(separator: "|")
+            return Rule(Int(parts[0])!, Int(parts[1])!)
+        })
         let updates = updatesInput.map { $0.split(separator: ",").map { Int($0)! }}
         return (rules, updates)
-    }
-
-    // returns a sort function based on the given rules
-    func rulesSorter(_ rules: [[Int]]) -> (Int, Int) -> Bool {
-        {
-            (a: Int, b: Int) -> Bool in
-                for rule in rules {
-                    if a == rule[0] && b == rule[1] {
-                        return true
-                    }
-                }
-                return false
-        }
     }
 
     override func part1(_ input: [String]) -> String {
@@ -37,7 +29,7 @@ class PrintQueue: Solution {
 
         var sum = 0
         for update in updates {
-            let sortedUpdate = update.sorted(by: rulesSorter(rules))
+            let sortedUpdate = update.sorted { rules.contains(Rule($0, $1)) }
             if sortedUpdate == update {
                 sum += update[update.count / 2]
             }
@@ -50,7 +42,7 @@ class PrintQueue: Solution {
 
         var sum = 0
         for update in updates {
-            let sortedUpdate = update.sorted(by: rulesSorter(rules))
+            let sortedUpdate = update.sorted { rules.contains(Rule($0, $1)) }
             if sortedUpdate != update {
                 sum += sortedUpdate[sortedUpdate.count / 2]
             }
