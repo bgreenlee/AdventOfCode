190e5bc52e18aa02df7a58e6d78d511a0a103b9a
Brad Greenlee <brad@footle.org>
2024-12-06 14:58:24 -0800

    cleanup

--- 2024/AdventOfCode/Solutions/Day 6/GuardGallivant.swift
+++ 2024/AdventOfCode/Solutions/Day 6/GuardGallivant.swift
@@ -13,6 +13,8 @@ class GuardGallivant: Solution {
         super.init(id: 6, name: "Guard Gallivant")
     }
 
+    // parse the input into a dict of Point -> Character, and
+    // return a tuple of that and the position of the guard
     func parseInput(_ input: [String]) -> ([Point: Character], Point) {
         var map: [Point: Character] = [:]
         var guardPos = Point(0, 0)
@@ -27,14 +29,8 @@ class GuardGallivant: Solution {
         return (map, guardPos)
     }
 
-    struct LoopDetectedError: Error {
-        let guardPos: Point
-        let direction: Vec
-    }
-
-    // run through the given map, returning when the guard goes off the map
-    // or throwing an exception if a loop is detected
-    func runMap(_ map: [Point: Character], guardPos: Point, direction: Vec = Vec(0, -1)) throws -> [Point: Character] {
+    // run through the given map, returning the updated map and whether a loop was detected
+    func runMap(_ map: [Point: Character], guardPos: Point, direction: Vec = Vec(0, -1)) -> ([Point: Character], Bool) {
         var map = map
         var guardPos = guardPos
         var direction = direction
@@ -43,7 +39,7 @@ class GuardGallivant: Solution {
         while map[guardPos] != nil {
             map[guardPos] = "v"
             guard !visited[guardPos, default: Set()].contains(direction) else {
-                throw LoopDetectedError(guardPos: guardPos, direction: direction)
+                return (map, true)
             }
             visited[guardPos, default: Set()].insert(direction)
             let newPos = guardPos &+ direction
@@ -54,38 +50,29 @@ class GuardGallivant: Solution {
             }
         }
 
-        return map
+        return (map, false)
     }
 
     override func part1(_ input: [String]) -> String {
         var (map, guardPos) = parseInput(input)
-        do {
-            map = try runMap(map, guardPos: guardPos)
-        } catch {
-            print("Error: \(error)") // this shouldn't happen in part 1
-        }
+        (map, _) = runMap(map, guardPos: guardPos)
         return String(map.values.filter { $0 == "v" }.count)
     }
 
     override func part2(_ input: [String]) -> String {
         var (map, guardPos) = parseInput(input)
-        var possibleLoops = 0
-        do {
-            map = try runMap(map, guardPos: guardPos)
-            let visitedPoints = map.keys.filter { map[$0] == "v" && $0 != guardPos }
-            // for each of the visitedPoints, try creating an obstruction and see if a loop is detected
-            for point in visitedPoints {
-                var newMap = map
-                newMap[point] = "#"
-                do {
-                    _ = try runMap(newMap, guardPos: guardPos)
-                } catch {
-                    possibleLoops += 1
-                }
+        var numLoops = 0
+        (map, _) = runMap(map, guardPos: guardPos) // initial run to populated visited points
+        let visitedPoints = map.keys.filter { map[$0] == "v" && $0 != guardPos }
+        // for each of the visitedPoints, try creating an obstruction and see if a loop is detected
+        for point in visitedPoints {
+            var newMap = map
+            newMap[point] = "#"
+            let (_, loopDetected) = runMap(newMap, guardPos: guardPos)
+            if loopDetected {
+                numLoops += 1
             }
-        } catch {
-            print("Error: \(error)") // this shouldn't happen the first time
         }
-        return String(possibleLoops)
+        return String(numLoops)
     }
 }
