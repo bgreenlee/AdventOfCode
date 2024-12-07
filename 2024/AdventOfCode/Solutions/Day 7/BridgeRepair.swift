81483a361fe4653e61cd6c51587c26435d9a09b0
Brad Greenlee <brad@footle.org>
2024-12-07 07:56:57 -0800

    Comments & use UInt

--- 2024/AdventOfCode/Solutions/Day 7/BridgeRepair.swift
+++ 2024/AdventOfCode/Solutions/Day 7/BridgeRepair.swift
@@ -10,26 +10,27 @@ class BridgeRepair: Solution {
         super.init(id: 7, name: "Bridge Repair")
     }
 
-    func parseInput(_ input: [String]) -> [(Int, [Int])] {
-        var equations: [(Int, [Int])] = []
+    func parseInput(_ input: [String]) -> [(UInt, [UInt])] {
+        var equations: [(UInt, [UInt])] = []
         for line in input {
             let parts = line.split(separator: ": ")
-            let testValue = Int(parts[0])!
-            let numbers = parts[1].split(separator: " ").map { Int($0)! }
+            let testValue = UInt(parts[0])!
+            let numbers = parts[1].split(separator: " ").map { UInt($0)! }
             equations.append((testValue, numbers))
         }
         return equations
     }
 
     struct OperatorCombinationsGenerator {
-        private var cache: [Int: [[Character]]] = [:] // cache of results for each k
+        private var cache: [UInt: [[Character]]] = [:]  // cache of results for each k
         private let operators: [Character]
 
         init(_ operators: [Character]) {
             self.operators = operators
         }
 
-        mutating func generate(_ k: Int) -> [[Character]] {
+        // generate all combinations of operators filling k slots
+        mutating func generate(_ k: UInt) -> [[Character]] {
             if let cached = cache[k] {
                 return cached
             }
@@ -38,7 +39,7 @@ class BridgeRepair: Solution {
 
             var result = operators.map { [$0] }
 
-            cache[1] = result // cache single-operator result
+            cache[1] = result  // cache single-operator result
 
             for length in 2...k {
                 result = result.flatMap { combo in
@@ -53,25 +54,30 @@ class BridgeRepair: Solution {
         }
     }
 
-    func calculate(_ numbers: [Int], _ operators: [Character]) -> Int {
-        var result = 0
-        let operators = ["+"] + operators
+    // given a list of n numbers and n - 1 operators, calculate the result
+    func calculate(_ numbers: [UInt], _ operators: [Character]) -> UInt {
+        guard numbers.count - operators.count == 1 else { return 0 }
+
+        var result: UInt = 0
+        let operators = ["+"] + operators // add a leading + so that we can zip
         for (num, op) in zip(numbers, operators) {
             switch op {
             case "+": result += num
             case "*": result *= num
-            case "|": result = Int(String(result) + String(num))!
+            case "|": result = UInt(String(result) + String(num))!
             default: break
             }
         }
         return result
     }
 
-    func scoreValidValues(equations: [(Int, [Int])], operators: [Character]) -> Int {
+    // given a list of equations and a set of operators, return the sum of all equations
+    // that can be satisfied by some combination of operators
+    func scoreValidValues(_ equations: [(UInt, [UInt])], _ operators: [Character]) -> UInt {
         var opGenerator = OperatorCombinationsGenerator(operators)
-        var result = 0
+        var result: UInt = 0
         for (testValue, numbers) in equations {
-            for ops in opGenerator.generate(numbers.count - 1) {
+            for ops in opGenerator.generate(UInt(numbers.count) - 1) {
                 if calculate(numbers, ops) == testValue {
                     result += testValue
                     break
@@ -83,11 +89,13 @@ class BridgeRepair: Solution {
 
     override func part1(_ input: [String]) -> String {
         let equations = parseInput(input)
-        return String(scoreValidValues(equations: equations, operators: ["+", "*"]))
+        let result = scoreValidValues(equations, ["+", "*"])
+        return String(result)
     }
 
     override func part2(_ input: [String]) -> String {
         let equations = parseInput(input)
-        return String(scoreValidValues(equations: equations, operators: ["+", "*", "|"]))
+        let result = scoreValidValues(equations, ["+", "*", "|"])
+        return String(result)
     }
 }
