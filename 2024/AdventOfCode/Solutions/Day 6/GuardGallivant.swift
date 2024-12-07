3f0634dc7fc9ae1f22aa51e16d256e97dacc2020
Brad Greenlee <brad@footle.org>
2024-12-06 18:53:20 -0800

    Parallelize the loop detection

--- 2024/AdventOfCode/Solutions/Day 6/GuardGallivant.swift
+++ 2024/AdventOfCode/Solutions/Day 6/GuardGallivant.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Brad Greenlee on 12/6/24.
 //
+import Dispatch
 
 typealias Point = SIMD2<Int>
 typealias Direction = SIMD2<Int>
@@ -30,7 +31,7 @@ class GuardGallivant: Solution {
     }
 
     // run through the given map, returning the list of visited points and whether a loop was detected
-    func runMap(_ map: [Point: Character], guardPos: Point, dir: Direction = Direction(0, -1))
+    func runMap(_ map: [Point: Character], guardPos: Point, dir: Direction = Direction(0, -1), newObstacle: Point? = nil)
         -> ([Point], Bool)
     {
         var guardPos = guardPos
@@ -43,7 +44,7 @@ class GuardGallivant: Solution {
             }
             visited[guardPos, default: []].append(dir)
             let newPos = guardPos &+ dir
-            if map[newPos] == "#" {
+            if newPos == newObstacle || map[newPos] == "#" {
                 dir = Direction(-dir.y, dir.x)  // rotate right 90 deg
             } else {
                 guardPos = newPos
@@ -60,20 +61,36 @@ class GuardGallivant: Solution {
         return String(visitedPoints.count)
     }
 
+    func parallelLoopDetection(map: [Point: Character], guardPos: Point, visitedPoints: [Point]) -> Int {
+        let counterQueue = DispatchQueue(label: "counterQueue")
+        var numLoops = 0
+
+        let group = DispatchGroup()
+
+        let pointsToCheck = visitedPoints.filter { $0 != guardPos }
+        DispatchQueue.concurrentPerform(iterations: pointsToCheck.count) { index in
+            let point = pointsToCheck[index]
+            let (_, loopDetected) = runMap(map, guardPos: guardPos, newObstacle: point)
+            if loopDetected {
+                group.enter()
+                counterQueue.async {
+                    numLoops += 1
+                    group.leave()
+                }
+            }
+        }
+
+        group.wait()
+
+        return numLoops
+    }
+
     override func part2(_ input: [String]) -> String {
         let (map, guardPos) = parseInput(input)
         let (visitedPoints, _) = runMap(map, guardPos: guardPos)  // initial run to populate visited points
 
-        // for each of the visitedPoints, try creating an obstruction and see if a loop is detected
-        var numLoops = 0
-        for point in visitedPoints.filter({ $0 != guardPos }) {
-            var newMap = map
-            newMap[point] = "#"
-            let (_, loopDetected) = runMap(newMap, guardPos: guardPos)
-            if loopDetected {
-                numLoops += 1
-            }
-        }
+        let numLoops = parallelLoopDetection(map: map, guardPos: guardPos, visitedPoints: visitedPoints)
+
         return String(numLoops)
     }
 }
