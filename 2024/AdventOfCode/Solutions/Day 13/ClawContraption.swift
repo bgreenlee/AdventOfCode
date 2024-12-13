ccc996e63b0ba7b9d45c6a215413a7ee7a6ae7c0
Brad Greenlee <brad@footle.org>
2024-12-13 13:56:48 -0800

    so the maxtrix math 'by hand' (thanks eviera!)

--- 2024/AdventOfCode/Solutions/Day 13/ClawContraption.swift
+++ 2024/AdventOfCode/Solutions/Day 13/ClawContraption.swift
@@ -12,7 +12,7 @@ class ClawContraption: Solution {
         super.init(id: 13, name: "Claw Contraption", hasDisplay: false)
     }
 
-    func parseInput(_ input: [String]) -> [([Double], [Double])] {
+    func parseInput(_ input: [String]) -> [(Int, Int, Int, Int, Int, Int)] {
         let matches =
             input
             .joined()
@@ -25,67 +25,24 @@ class ClawContraption: Solution {
                     /#
             )
         return matches.map {
-            (
-                // matrix of coefficients
-                [
-                    Double($0.1)!, Double($0.2)!,
-                    Double($0.3)!, Double($0.4)!,
-                ],
-                // vector of answers
-                [
-                    Double($0.5)!,
-                    Double($0.6)!,
-                ]
-            )
+            (Int($0.1)!, Int($0.2)!, Int($0.3)!, Int($0.4)!, Int($0.5)!, Int($0.6)!)
         }
     }
 
-    // from https://developer.apple.com/documentation/accelerate/solving_systems_of_linear_equations_with_lapack
-    func solveSystemOfEquations(_ a: [Double], _ b: [Double]) -> [Double]? {
-        let dimension = b.count  // technically: Int(sqrt(Double(a.count)))
-        let rightHandSideCount = 1
-        var info: __LAPACK_int = 0
-
-        /// Create a mutable copy of the right hand side matrix _b_ that the function returns as the solution matrix _x_.
-        var x = b
-
-        /// Create a mutable copy of `a` to pass to the LAPACK routine. The routine overwrites `mutableA`
-        /// with the factors `L` and `U` from the factorization `A = P * L * U`.
-        var mutableA = a
-
-        var ipiv = [__LAPACK_int](repeating: 0, count: dimension)
-
-        /// Call `dgesv_` to compute the solution.
-        withUnsafePointer(to: __LAPACK_int(dimension)) { n in
-            withUnsafePointer(to: __LAPACK_int(rightHandSideCount)) { nrhs in
-                dgesv_(n, nrhs, &mutableA, n, &ipiv, &x, n, &info)
-            }
-        }
-
-        if info != 0 {
-            print("solveSystemOfEquations error \(info)")
-            return nil
-        }
-        return x
-    }
-
-    func solve(_ input: [String], max: Double = 0, add: Double = 0) -> Int {
+    func solve(_ input: [String], max: Double = 0, add: Int = 0) -> Int {
         let configurations = parseInput(input)
         var total: Int = 0
-        for (matA, vecB) in configurations {
-            let vecB = vecB.map { $0 + add }
-            if let result = solveSystemOfEquations(matA, vecB) {
-                let a = result[0]
-                let b = result[1]
-                if a < 0 || b < 0
-                    || (max > 0 && (a > max || b > max))
-                    || !a.isApproximatelyEqual(to: a.rounded(), absoluteTolerance: 0.001)
-                    || !b.isApproximatelyEqual(to: b.rounded(), absoluteTolerance: 0.001)
-                {
-                    continue  // not a valid solution
-                }
-                total += 3 * Int(a.rounded()) + Int(b.rounded())
+        for (ax, ay, bx, by, px, py) in configurations {
+            let (px, py) = (add + px, add + py)
+            let b = Double(ay * px - ax * py) / Double(ay * bx - ax * by)
+            let a = Double(Double(px) - b * Double(bx)) / Double(ax)
+            if a < 0 || b < 0 || (max > 0 && (a > max || b > max))
+                || !a.isApproximatelyEqual(to: a.rounded(), absoluteTolerance: 0.001)
+                || !b.isApproximatelyEqual(to: b.rounded(), absoluteTolerance: 0.001)
+            {
+                continue  // not a valid solution
             }
+            total += 3 * Int(a) + Int(b)
         }
         return total
     }
